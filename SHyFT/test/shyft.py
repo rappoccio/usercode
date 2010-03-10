742aa8c0741fbd9558249d2218cf45e264f413da
srappocc <>
2010-03-10 05:10:37 +0000

    Adding trigger matching to std sequence

--- SHyFT/test/shyft.py
+++ SHyFT/test/shyft.py
@@ -33,10 +33,6 @@ process.patTaus.isoDeposits = cms.PSet()
 process.electronMatch.checkCharge = False
 process.muonMatch.checkCharge = False
 
-# switch to 8e29 menu. Note this is needed to match SD production
-##process.patTriggerEvent.processName = cms.string( 'HLT8E29' )
-##process.patTrigger.processName = cms.string( 'HLT8E29' )
-
 ## produce ttGenEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 
@@ -62,6 +58,10 @@ from PhysicsTools.PatAlgos.tools.trigTools import *
 switchOnTrigger( process )
 from PhysicsTools.PatAlgos.patEventContent_cff import patTriggerEventContent
 
+# switch to 8e29 menu. Note this is needed to match SD production
+#process.patTriggerEvent.processName = cms.string( 'HLT8E29' )
+#process.patTrigger.processName = cms.string( 'HLT8E29' )
+
 #try to clone what we have up to here.  
 from PhysicsTools.PatAlgos.tools.helpers import cloneProcessingSnippet
 cloneProcessingSnippet(process, process.patDefaultSequence, "Std")
@@ -72,7 +72,6 @@ usePF2PAT(process,runPF2PAT=True, jetAlgo='AK5')
 
 # remove trigger matching for PF2PAT as that is currently broken
 process.patDefaultSequence.remove(process.patTriggerSequence)
-
 
 
 # end changes for PF
@@ -102,8 +101,18 @@ process.out.outputCommands += ['drop *_cleanLayer1*_*_*',
 			       'keep *_offlineBeamSpot_*_*',
 			       'keep *_offlinePrimaryVertices_*_*',
                                'keep recoTracks_generalTracks_*_*',
-                               'drop patPFParticles_*_*_*']
-process.out.outputCommands += patTriggerEventContent
+                               'drop patPFParticles_*_*_*',
+                               'keep patTriggerObjects_patTriggerStd_*_*',
+                               'keep patTriggerFilters_patTriggerStd_*_*',
+                               'keep patTriggerPaths_patTriggerStd_*_*',
+                               'keep patTriggerEvent_patTriggerEventStd_*_*',
+                               'keep *_cleanPatPhotonsTriggerMatchStd_*_*',
+                               'keep *_cleanPatElectronsTriggerMatchStd_*_*',
+                               'keep *_cleanPatMuonsTriggerMatchStd_*_*',
+                               'keep *_cleanPatTausTriggerMatchStd_*_*',
+                               'keep *_cleanPatJetsTriggerMatchStd_*_*',
+                               'keep *_patMETsTriggerMatchStd_*_*'
+]
 process.out.dropMetaData = cms.untracked.string("DROPPED")
 process.out.fileName = 'ljmet.root'
 process.options.wantSummary = True
