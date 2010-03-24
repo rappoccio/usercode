16023bde9f05ff6471969acf24a5410d4de7e67f
srappocc <>
2010-03-24 19:03:20 +0000

    Migrated to SelectorUtils

--- AnalysisFilters/python/pvSelector_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-
-pvSelector = cms.PSet(
-    pvSrc = cms.InputTag('offlinePrimaryVertices'),
-    minPVNdof = cms.double(5.0),
-    maxPVZ = cms.double(15.0)
-    )
