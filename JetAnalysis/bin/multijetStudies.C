04b6d15787d874ba58c1ac65a7e98e5746b26f9a
srappocc <>
2010-04-02 14:51:29 +0000

    Use the same selector with configuration changes as DijetAnalyiss

--- JetAnalysis/bin/multijetStudies.C
+++ JetAnalysis/bin/multijetStudies.C
@@ -45,171 +45,13 @@ using namespace std;
 #include "DataFormats/Common/interface/Ptr.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "PhysicsTools/SelectorUtils/interface/EventSelector.h"
-
+#include "Analysis/JetAnalysis/interface/DijetSelector.h"
 
 // Root includes
 #include "TROOT.h"
 
 using namespace std;
 
-
-class MultijetSelector : public EventSelector {
-public:
-  MultijetSelector( edm::ParameterSet const & caloJetIdParams,
-		    edm::ParameterSet const & pfJetIdParams,
-		    edm::ParameterSet const & params ) :
-    jetSel_    (new JetIDSelectionFunctor(caloJetIdParams)), 
-    pfJetSel_  (new PFJetIDSelectionFunctor(pfJetIdParams)),
-    jetSrc_    (params.getParameter<edm::InputTag>("jetSrc")), 
-    pfJetSrc_  (params.getParameter<edm::InputTag>("pfJetSrc")),
-    minNJets_  (params.getParameter<int>("minNJets")),
-    ptMin_     (params.getParameter<double>("ptMin"))
-  {
-    bool useCalo = params.getParameter<bool>("useCalo");
-	       
-    push_back("Calo Cuts");
-    push_back("Calo Kin Cuts");
-    push_back("Calo Jet ID");
-    push_back("PF Cuts");
-    push_back("PF Kin Cuts");
-    push_back("PF Jet ID");
-
-    set("Calo Cuts", useCalo);
-    set("Calo Kin Cuts", useCalo);
-    set("Calo Jet ID", useCalo);
-    set("PF Cuts", !useCalo);
-    set("PF Kin Cuts", !useCalo);    
-    set("PF Jet ID", !useCalo);
-    
-  }
-
-  virtual ~MultijetSelector() {}
-
-  virtual bool operator()( edm::EventBase const & event, std::strbitset & ret){
-    allCaloJets_.clear();
-    allPFJets_.clear();
-    caloJets_.clear();
-    pfJets_.clear();
-
-    std::strbitset retCaloJet = jetSel_->getBitTemplate();
-    std::strbitset retPFJet = pfJetSel_->getBitTemplate();
-
-    if ( considerCut("Calo Cuts") ) {
-      passCut(ret, "Calo Cuts");
-      event.getByLabel( jetSrc_, h_jets_ );
-      // Calo Cuts
-
-      int ncalo = 0;
-      if ( h_jets_->size() >= minNJets_ ) {
-	for (vector<pat::Jet>::const_iterator jetsBegin = h_jets_->begin(),
-	       jetsEnd = h_jets_->end(),
-	       ijet = jetsBegin;
-	     ijet != jetsEnd; ++ijet ) {
-	  if ( ijet->pt() > ptMin_ ) {
-	    allCaloJets_.push_back( edm::Ptr<pat::Jet>( h_jets_, ijet - jetsBegin ) );
-	  }
-	}
-      }
-	
-      if ( allCaloJets_.size() >= minNJets_ || ignoreCut("Calo Kin Cuts") ) {
-	passCut(ret, "Calo Kin Cuts");
-
-	for (vector<edm::Ptr<pat::Jet> >::const_iterator jetsBegin =allCaloJets_.begin(),
-	       jetsEnd = allCaloJets_.end(),
-	       ijet = jetsBegin;
-	     ijet != jetsEnd; ++ijet ) {
-	  pat::Jet const & jet = **ijet;
-	  retCaloJet.set(false);
-	  bool pass0 = (*jetSel_)( jet, retCaloJet );
-	  if ( pass0 ) 
-	    caloJets_.push_back( *ijet );
-	}
-
-	if ( caloJets_.size() >= minNJets_ || ignoreCut("Calo Jet ID") ) {
-	  passCut(ret, "Calo Jet ID");
-	}
-      }// end calo kin cuts
-    }// end if calo cuts
-
-
-
-    if ( considerCut("PF Cuts") ) {
-      passCut(ret, "PF Cuts");
-      event.getByLabel( pfJetSrc_, h_pfjets_ );
-      // PF Cuts
-
-      int npf = 0;
-      if ( h_pfjets_->size() >= minNJets_ ) {
-	for (vector<pat::Jet>::const_iterator jetsBegin = h_pfjets_->begin(),
-	       jetsEnd = h_pfjets_->end(),
-	       ijet = jetsBegin;
-	     ijet != jetsEnd; ++ijet ) {
-	  if ( ijet->pt() > ptMin_ ) {
-	    allPFJets_.push_back( edm::Ptr<pat::Jet>( h_pfjets_, ijet - jetsBegin ) );
-	  }
-	}
-      }
-	
-      if ( allPFJets_.size() >= minNJets_ || ignoreCut("PF Kin Cuts") ) {
-	passCut(ret, "PF Kin Cuts");
-
-	for (vector<edm::Ptr<pat::Jet> >::const_iterator jetsBegin =allPFJets_.begin(),
-	       jetsEnd = allPFJets_.end(),
-	       ijet = jetsBegin;
-	     ijet != jetsEnd; ++ijet ) {
-	  pat::Jet const & jet = **ijet;
-	  retPFJet.set(false);
-	  bool pass0 = (*pfJetSel_)( jet, retPFJet );
-	  if ( pass0 ) 
-	    pfJets_.push_back( *ijet );
-	}
-
-	if ( pfJets_.size() >= minNJets_ || ignoreCut("PF Jet ID") ) {
-	  passCut(ret, "PF Jet ID");
-	}
-      }// end pf kin cuts
-    }// end if pf cuts
-
-
-
-
-
-    setIgnored(ret);
-
-    return false;
-  }// end of method
-
-  boost::shared_ptr<JetIDSelectionFunctor> const &   jetSel()     const { return jetSel_;}
-  boost::shared_ptr<PFJetIDSelectionFunctor> const & pfJetSel()   const { return pfJetSel_;}
-
-  std::vector<edm::Ptr<pat::Jet> > const &  allCaloJets () const { return allCaloJets_; }
-  std::vector<edm::Ptr<pat::Jet> > const &  allPFJets   () const { return allPFJets_; }
-
-  std::vector<edm::Ptr<pat::Jet> > const &  caloJets() const { return caloJets_; }
-  std::vector<edm::Ptr<pat::Jet> > const &  pfJets  () const { return pfJets_; }
-
-
-
-protected:
-  boost::shared_ptr<JetIDSelectionFunctor>   jetSel_;
-  boost::shared_ptr<PFJetIDSelectionFunctor> pfJetSel_;
-  edm::InputTag                              jetSrc_;
-  edm::InputTag                              pfJetSrc_;
-  int                                        minNJets_;
-  double                                     ptMin_;
-
-
-  edm::Handle<std::vector<pat::Jet> >        h_jets_;
-  edm::Handle<std::vector<pat::Jet> >        h_pfjets_;
-  
-  std::vector<edm::Ptr<pat::Jet> >           allCaloJets_;
-  std::vector<edm::Ptr<pat::Jet> >           allPFJets_;
-
-
-  std::vector<edm::Ptr<pat::Jet> >           caloJets_;
-  std::vector<edm::Ptr<pat::Jet> >           pfJets_;
-
-};
 
 ///////////////////////////
 // ///////////////////// //
@@ -336,16 +178,24 @@ int main (int argc, char* argv[])
   hists["hist_pf_good_nConstituents"  ] = theDir.make<TH1D>( "hist_pf_good_nConstituents" , "PFJet nConstituents", 20, 0, 20 ) ;
 
    
-   
+  
+
+  // Now can use the "DijetSelector" as a MultijetSelector, if we ignore
+  // the delta Phi cut. 
   cout << "Making functors" << endl;
-  MultijetSelector caloSelector( caloJetIDParameters,
-				 pfJetIDParameters,
-				 jetStudiesParams );
+  DijetSelector caloSelector( caloJetIDParameters,
+			      pfJetIDParameters,
+			      jetStudiesParams );
 
-  MultijetSelector pfSelector( caloJetIDParameters,
-			       pfJetIDParameters,
-			       pfJetStudiesParams );
-
+  caloSelector.set("Calo Delta Phi", false);
+  caloSelector.set("PF Delta Phi", false);
+  
+  DijetSelector pfSelector( caloJetIDParameters,
+			    pfJetIDParameters,
+			    pfJetStudiesParams );
+  pfSelector.set("Calo Delta Phi", false);
+  pfSelector.set("PF Delta Phi", false);
+  
   vector<int> const & runs = plotParameters.getParameter<std::vector<int> >("runs");
   bool doTracks = plotParameters.getParameter<bool>("doTracks");
   bool useMC    = plotParameters.getParameter<bool>("useMC");
--- JetAnalysis/bin/multijetStudies_cfg.py
+++ JetAnalysis/bin/multijetStudies_cfg.py
@@ -9,7 +9,7 @@ process.jetStudies = cms.PSet(
     metSrc = cms.InputTag('patMETs'),
     pfMetSrc = cms.InputTag('patMETsPF'),
     useCalo = cms.bool(True),
-    minNJets = cms.int32(2),
+    minNJets = cms.int32(4),
     ptMin = cms.double(10.0)
 )
 process.pfJetStudies = process.jetStudies.clone( useCalo = cms.bool(False),
@@ -28,17 +28,17 @@ process.plotParameters = cms.PSet (
 
 process.inputs = cms.PSet (
     fileNames = cms.vstring(
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_1.root',
-#'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_10.root',
-#'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_11.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_2.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_3.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_4.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_5.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_6.root',
-'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_7.root'
-#'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_8.root',
-#'/uscms/home/rappocc/nobackup/data/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_9.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_1.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_10.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_11.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_2.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_3.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_4.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_5.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_6.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_7.root'
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_8.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_9.root'
 
         )
 )
