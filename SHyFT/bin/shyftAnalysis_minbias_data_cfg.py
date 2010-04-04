439d0f0a49d42bed11f0e212befae0b18cd9fdd5
srappocc <>
2010-04-04 01:46:58 +0000

    Adding files to run over data and ttbar preprod

---
+++ SHyFT/bin/shyftAnalysis_minbias_data_cfg.py
@@ -0,0 +1,34 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("FWLitePlots")
+
+
+process.load('Analysis.SHyFT.shyftAnalysis_cfi')
+
+
+process.shyftAnalysis.muSrc = cms.InputTag('selectedPatMuons')
+process.shyftAnalysis.electronSrc = cms.InputTag('selectedPatElectrons')
+process.shyftAnalysis.jetSrc = cms.InputTag('selectedPatJets')
+process.shyftAnalysis.metSrc = cms.InputTag('patMETs')
+process.shyftAnalysis.trigSrc = cms.InputTag('patTriggerEvent')
+
+
+process.inputs = cms.PSet (
+    fileNames = cms.vstring(
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_1.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_10.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_11.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_2.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_3.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_4.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_5.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_6.root',
+'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_7.root'
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_8.root',
+#'/Users/rappocc/data_local/MinimumBias/pat_7TeV_minbias_data_v2/reco_7TeV_firstdata_356_pat_9.root'
+        )
+)
+
+process.outputs = cms.PSet (
+    outputName = cms.string('shyftPlots_minbias_data.root')
+)
