69f1425ea9069adac5040ca60568bed8d5ebebff
srappocc <>
2010-04-23 18:21:40 +0000

    Adding updates for data and synchronization exercise

--- SHyFT/bin/shyftAnalysis_ttbar_syncex_cfg.py
+++ SHyFT/bin/shyftAnalysis_ttbar_syncex_cfg.py
@@ -9,8 +9,9 @@ process.load('Analysis.SHyFT.shyftAnalysis_cfi')
 process.inputs = cms.PSet (
     maxEvents = cms.int32(-1),
     fileNames = cms.vstring(
-'/uscms_data/d2/rappocc/SHyFTFiles/TTbar/shyft_35x_v3_syncex/ljmet_1_1.root',
-'/uscms_data/d2/rappocc/SHyFTFiles/TTbar/shyft_35x_v3_syncex/ljmet_2_1.root'
+'dcap:///pnfs/cms/WAX/11/store/user/rappocc/TTbar/shyft_35x_v4_syncex/b0399e0e9cf131396b9de602835507f7/ljmet_1_1.root',
+'dcap:///pnfs/cms/WAX/11/store/user/rappocc/TTbar/shyft_35x_v4_syncex/b0399e0e9cf131396b9de602835507f7/ljmet_2_1.root'
+
 
 
         )
