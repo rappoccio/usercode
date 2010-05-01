033518edc0a92d3648d1d85f602b99cae2a1fed6
dfehling <>
2010-05-01 23:39:51 +0000

    It's alive! Now works, but needs substantial clean-up. Use this file for now:combined_histos_v2_minJets1_jes100.root

--- SHyFT/test/new_ragnarok.C
+++ SHyFT/test/new_ragnarok.C
@@ -59,11 +59,12 @@ private:
   //  RooFormulaVar * form_;
   RooRealVar  binningVar_;
   //  RooRealVar * lum_;
+  double form_value_;
 };
 
 Template::Template(TH1F hist, string name, RooRealVar& binningVar, vector<RooRealVar*> scaleFactors)://, RooRealVar& lum, RooRealVar & kFactor):
   hist_(hist),name_(name),binningVar_(binningVar) {//,lum_(scaleFactors[1]) {
-  nmc_   = hist_.Integral();
+  nmc_   = hist_.GetEntries();
   cout << "Number of MC events of " << name_.c_str() << " is " << nmc_ << endl;
   norm_    = new RooRealVar ((name_+"_norm").c_str(), (name_+"_norm").c_str(),nmc_);//,-1,100000);
   rdh_     = new RooDataHist(name_.c_str(),name_.c_str(),binningVar_, &hist_);
@@ -72,6 +73,8 @@ Template::Template(TH1F hist, string name, RooRealVar& binningVar, vector<RooRea
   //In the ScaleFactors, 0 is the xsection/fit variable, 1 is the luminosity, 2 is the generated xsection, and 3 is the number of events
   RooFormulaVar * form_ = new RooFormulaVar((name_+"_form").c_str(), (name_+"_form").c_str(),"@0*@1*@2*@3/@4",
 					    RooArgSet(*scaleFactors[0], *norm_,*scaleFactors[1],*scaleFactors[2],*scaleFactors[3]));//, lum, kFactor));
+  form_value_ = form_->evaluate();
+  std::cout << "formula value is " << form_value_ << endl;
   rep_   = new RooExtendPdf((name_+"_epdf").c_str(), (name_+"_epdf").c_str(), *rhp_, *form_);
 
   expHist_ = 0;
@@ -91,16 +94,18 @@ Template::~Template() {
 
 class JetTagBin {
 public:
-  JetTagBin(TFile & file, vector<string> const & samples, string jtBinName, scaleFact_vect scaleFactor);//map<string, RooRealVar *> scaleFactors);
+  JetTagBin(TFile & file, vector<string> const & samples, string jtBinName, scaleFact_vect scaleFactor);
+  //map<string, RooRealVar *> scaleFactors);
   ~JetTagBin();
   vector<Template*> & jtTemplates() { return jtTemplates_; }
   
   RooNLLVar * nll()  { return jtNll_; } 
 
-  void setupPE( int N );
+  void setupPE( double nent );
   void inputData( TFile * file );
   void makeNLLVar();
   void plotOn( RooPlot * frame );
+  RooAbsData * jtData() {return jtData_;}
 
   void setKFactors( vector<double> const & inputKFactors ) {
     kFactors_.clear();
@@ -141,11 +146,17 @@ JetTagBin::JetTagBin(TFile & file, vector<string> const & samples, string jtBinN
     }
     else
       tempHisto2 = (TH1F*) tempHisto->Clone();
-
     Template * temp = new Template(*tempHisto2, name, *jtFitVar_, scaleFactors[samples[i]]);//, kFactors_[i]]);
     jtTemplates_.push_back( temp );
     kFactors_.push_back(1.0);
   }
+
+  int nbins = jtTemplates_[0]->hist()->GetNbinsX();
+  int xlo   = jtTemplates_[0]->hist()->GetBinLowEdge(1);
+  int xhi   = jtTemplates_[0]->hist()->GetBinLowEdge(nbins + 1);
+  RooUniformBinning jtBinning(xlo,xhi,nbins);
+  jtFitVar_->setBinning(jtBinning);
+
   RooArgList TemplateList;
   //RooArgList NormList;
   if(verbose) cout << "making lists" << endl;
@@ -162,14 +173,14 @@ JetTagBin::JetTagBin(TFile & file, vector<string> const & samples, string jtBinN
 }
 
 
-void JetTagBin::setupPE( int N )
+void JetTagBin::setupPE( double Lum )
 {
   if(verbose) cout << "generating pseudoexperiment" << endl;
 
-
-
   if ( jtData_ ) delete jtData_;
-  jtData_ = jtPdf_->generate( RooArgList(*jtFitVar_), N, Extended() );
+  jtData_ = jtPdf_->generateBinned( RooArgList(*jtFitVar_), Verbose(1));
+
+  if(verbose) cout << "generated pseudoexperiment with " << jtData_->numEntries() << " entries" << endl;
 }
 
 
@@ -188,7 +199,7 @@ void JetTagBin::makeNLLVar()
 {
   if(verbose) cout << "creating nllVar" << endl;
   if ( jtNll_ ) delete jtNll_;
-  jtNll_ = new RooNLLVar((jtBinName_+"_nll").c_str(), (jtBinName_+"_nll").c_str(), *jtPdf_, *jtData_);
+  jtNll_ = new RooNLLVar((jtBinName_+"_nll").c_str(), (jtBinName_+"_nll").c_str(), *jtPdf_, *jtData_, Extended);
 }
 
 void JetTagBin::plotOn( RooPlot * frame )
@@ -209,11 +220,11 @@ public:
   SHyFT( string const & fileName, double lum );
   ~SHyFT() { file_->Close(); }
   
-  void generatePEs( int N ); 
+  void generatePEs( double Lum ); 
   void makeNLLVars();
   void fit(bool verbose);
   void print();
-  void plot( double lum );
+  void plot( RooFitResult * result );
   RooFitResult * fitResult() { return fitResult_; }
 
   void setKFactors( vector<double> const & inputKFactors ) {
@@ -274,18 +285,35 @@ SHyFT::SHyFT( string const & fileName, double lum ) :
   wjetsSF.push_back(&genXsec_wjets);
   wjetsSF.push_back(&genNevt_wjets);
 
-  scaleFactors_["ttbar"]=ttbarSF;
-  scaleFactors_["WJets"]=wjetsSF;
+  scaleFactors_["Top"]=ttbarSF;
+  scaleFactors_["Wjets"]=wjetsSF;
+  //  scaleFactors_["ttbar"]=ttbarSF;
+  //scaleFactors_["WJets"]=wjetsSF;
   
   //  scaleFactors_["ttbar"]=&ttbar_xsec_;
   //scaleFactors_["WJets"]=&WJets_xsec_;
 
-  samples_.push_back("ttbar");//_hT_");
-  samples_.push_back("WJets");//_hT_");
+  //samples_.push_back("ttbar");//_hT_");
+  //samples_.push_back("WJets");//_hT_");
+  samples_.push_back("Top");//_hT_");
+  samples_.push_back("Wjets");//_hT_");
 
+  //Create binning
+  /*map<string, RooUniformBinning * > theBinning;
+  theBinning["_hT_1j"] = new RooUniformBinning( 50,300,10);
+  theBinning["_hT_2j"] = new RooUniformBinning( 80,500,10);
+  theBinning["_hT_3j"] = new RooUniformBinning(110,600,10);
+  theBinning["_hT_4j"] = new RooUniformBinning(140,600, 5);
+  theBinning["_hT_5j"] = new RooUniformBinning(170,600, 5);
+  RooUniformBinning bin_hT_2j( 80,500,10);
+  RooUniformBinning bin_hT_3j(110,600,10);
+  RooUniformBinning bin_hT_4j(140,600, 5);
+  RooUniformBinning bin_hT_5j(170,600, 5);
+  */
+  
   stringstream tmpString;
   
-  for(int n_jets = 1; n_jets <=5; ++n_jets) { //will run only once
+  for(int n_jets = 1; n_jets <=5; ++n_jets) {
     //    for(int n_tags = 1; n_tags < 2; ++ n_tags) { // will run only once for now, but must run later until 3
     //      if(n_tags > n_jets) continue;
       tmpString.str("");
@@ -306,11 +334,11 @@ void SHyFT::print()
   }
 }
 
-void SHyFT::generatePEs(int N)
+void SHyFT::generatePEs(double Lum)
 {
   if(verbose) cout << "Generating Pseudoexperiments" << endl;
   for(unsigned int i=0;i<bins_.size();++i) {
-    bins_[i]->setupPE(N);
+    bins_[i]->setupPE( Lum );
   }
 }
 
@@ -331,7 +359,24 @@ void SHyFT::fit(bool verbose)
 						 *bins_[3]->nll(),
 						 *bins_[4]->nll()
 						 ) );
+  nllsum.printMetaArgs(cout);
+  nllsum.Print("v");
+  nllsum.printValue(cout);
+  RooNLLVar * test_nll = (RooNLLVar *) nllsum.FindObject("_hT_1j_nll");
+  cout << endl << endl;
+  if(test_nll!=0)
+    {
+      test_nll->printArgs(cout);
+      cout << endl;
+      //  RooExtendPdf * test_pdf = (RooExtendPdf *) test_nll.findObject("
+      RooRealVar * test_ttbar_norm = (RooRealVar *) (*test_nll).FindObject("ttbar_hT_1j_norm");
+    }
+  
+  //cout << "test_ttbar_norm = " << test_ttbar_norm->getVal() << endl;
+  //        double ttbar_xsec = ((RooRealVar * )(*results.at(i)).floatParsFinal().find("ttbar_hT_xsec"))->getVal();
+
   RooMinuit m(nllsum);
+  //  m.findObject(
   if(verbose){
     std::cout << "VERBOSE" << std::endl;
     m.setVerbose(kTRUE);
@@ -341,12 +386,13 @@ void SHyFT::fit(bool verbose)
     m.setPrintLevel(-1);
   }
   m.migrad();
+  //m.hesse();
   fitResult_ = m.save() ;
   // Print the fit result snapshot
   if(verbose) fitResult_->Print("v") ;
 }
 
-void SHyFT::plot(double lum)
+void SHyFT::plot(RooFitResult * result)
 {
   //plotting stuff and examination below here
   if(verbose) cout << "Plotting" << endl;
@@ -359,16 +405,29 @@ void SHyFT::plot(double lum)
     JetTagBin & bin = **ibin;
     TCanvas * c1 = new TCanvas(("c"+bin.jtBinName()).c_str(), "Combined Fit");
     RooPlot *frame = bin.jtFitVar()->frame();
-
-    double scale0 = bin.jtTemplates().at(0)->nmc()*(bin.kFactors().at(0))*lum;
-    double scale1 = bin.jtTemplates().at(1)->nmc()*(bin.kFactors().at(1))*lum;
-    bin.jtTemplates().at(0)->epdf()->plotOn(frame, LineColor(3),Normalization(scale0));
-    bin.jtTemplates().at(1)->epdf()->plotOn(frame, LineColor(2),Normalization(scale1));
+    double ttbar_xsec_gen = ((RooRealVar * ) result->constPars().find("genXsec_ttbar"))->getVal();
+    double luminosity = ((RooRealVar * ) result->constPars().find("luminosity"))->getVal();
+    double ttbar_xsec_norm = ((RooRealVar * ) result->floatParsFinal().find("ttbar_hT_xsec"))->getVal();
+    double ttbar_xsec_nevt = ((RooRealVar * ) result->constPars().find("genNevt_ttbar"))->getVal();
+    double norm_ttbar = bin.jtTemplates()[0]->nmc();
+    double norm_wjets = bin.jtTemplates()[1]->nmc();
+    //    double lum = ((RooRealVar * ) result->floatParsFinal().find("ttbar_hT_xsec"))->getVal();
+    double wjets_xsec_norm = ((RooRealVar * ) result->floatParsFinal().find("WJets_hT_xsec"))->getVal();
+    double wjets_xsec_nevt = ((RooRealVar * ) result->constPars().find("genNevt_wjets"))->getVal();
+    double wjets_xsec_gen = ((RooRealVar * ) result->constPars().find("genXsec_wjets"))->getVal();
+    std::cout << ttbar_xsec_norm << " " << wjets_xsec_norm << endl;
+//    double scale0 = bin.jtTemplates().at(0)->nmc()*(bin.kFactors().at(0))*lum;
+    //double scale1 = bin.jtTemplates().at(1)->nmc()*(bin.kFactors().at(1))*lum;
     bin.plotOn( frame );
+    //    bin.jtTemplates().at(0)->epdf()->plotOn(frame, ProjWData(*(bin.jtData())), LineColor(2));
+    //bin.jtTemplates().at(1)->epdf()->plotOn(frame, ProjWData(*(bin.jtData())), LineColor(3));
+    bin.jtTemplates().at(0)->epdf()->plotOn(frame, LineColor(2),Normalization(ttbar_xsec_norm*ttbar_xsec_gen*luminosity*norm_ttbar/ttbar_xsec_nevt,RooAbsReal::NumEvent));
+    bin.jtTemplates().at(1)->epdf()->plotOn(frame, LineColor(3),Normalization(wjets_xsec_norm*wjets_xsec_gen*luminosity*norm_wjets/wjets_xsec_nevt,RooAbsReal::NumEvent));
     //jtPdf_->plotOn(frame,Components("ttbar_hT_1j_pdf"),LineColor(3));
     //jtPdf_->plotOn(frame,Components("WJets_hT_1j_pdf"),LineColor(2));
     frame->Draw();
     c1->SaveAs(("fitter"+bin.jtBinName()+".png").c_str());
+    delete c1;
   }
   //jtFitVar_->Print("t");
   //jtPdf_->Print("v");
@@ -400,10 +459,11 @@ void Fitter(string fileName, int maxPEs)
   shyft.setKFactors( kFactors );
   for(int numPEs = 0; numPEs < maxPEs; ++numPEs) {
 
-    shyft.generatePEs(1);
+    shyft.generatePEs(lum);
     shyft.makeNLLVars();
     if(verbose) shyft.print();
     shyft.fit(verbose);
+    if(verbose) shyft.plot( shyft.fitResult() );
     results.push_back(shyft.fitResult());
     cout << "NEVENT IS " << numPEs << endl;
   }
@@ -432,9 +492,6 @@ void Fitter(string fileName, int maxPEs)
   ttbar_xsec_avg/=results.size();
   wjets_xsec_avg/=results.size();
 
-  cout << " ttbar_xsec = " << ttbar_xsec_avg << endl;//" times gen, or = " << 
-  cout << " wjets_xsec = " << wjets_xsec_avg << endl;//" times gen, or = " << 
-  //shyft.plot( lum );
   //  TCanvas * c1 = new TCanvas("ttbar_pulls", "ttbar_pulls");
   TCanvas * c1 = new TCanvas("plots", "plots");
   //pull_ratio->Draw();
@@ -447,6 +504,7 @@ void Fitter(string fileName, int maxPEs)
   c1->SaveAs("ttbar_pull_100.png");
   wjets_pull_h1->Draw();
   c1->SaveAs("wjets_pull_100.png");
+  delete c1;
 
 }
 
