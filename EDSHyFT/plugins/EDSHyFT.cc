7c08c6c699ee6abe185ef2cee628c8717f5c0625
srappocc <>
2010-08-28 04:05:33 +0000

    Major shift, move all framework files to EDSHyFT from SHyFT. Also add JPT and tcMET to the pat-tuples

--- EDSHyFT/plugins/EDSHyFT.cc
+++ EDSHyFT/plugins/EDSHyFT.cc
@@ -4,14 +4,15 @@
 
 #include "Analysis/SHyFT/interface/SHyFT.h"
 #include "PhysicsTools/UtilAlgos/interface/FWLiteAnalyzerWrapper.h"
+#include "PhysicsTools/UtilAlgos/interface/FWLiteFilterWrapper.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 
 
 typedef edm::FWLiteAnalyzerWrapper<SHyFT> EDSHyFT;
-
+typedef edm::FWLiteFilterWrapper<WPlusJetsEventSelector> EDWPlusJets;
 
 DEFINE_FWK_MODULE(EDSHyFT);
-
+DEFINE_FWK_MODULE(EDWPlusJets);
 
 #endif
--- EDSHyFT/test/shyftEDAnalyzer.py
+++ EDSHyFT/test/shyftEDAnalyzer.py
@@ -11,11 +11,11 @@ process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-        'dcap:///pnfs/cms/WAX/11/store/user/lannon/TTbarJets-madgraph/SHYFT_pat357_skim/0aa0bf8c0747525f693ade731a97f67d/ljmet_skim_7_1.root'
+                                'dcap:///pnfs/cms/WAX/11/store/user/rappocc/TTbarJets-madgraph/shyft_38xOn356_v2/efe53b645403790e1d3a86e1908711f6/shyft_382_pat_10_1_b1r.root'
     )
 )
 ## Maximal Number of Events
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 from Analysis.SHyFT.shyftAnalysis_cfi import shyftAnalysis as inputShyftAnalysis
 
@@ -25,13 +25,33 @@ process.TFileService = cms.Service("TFileService",
                                    )
 
 
-process.shyftAna = cms.EDAnalyzer('EDSHyFT',
-                                  shyftAnalysis = inputShyftAnalysis.clone()
-                                     )
+process.pfShyftAna = cms.EDAnalyzer('EDSHyFT',
+                                  shyftAnalysis = inputShyftAnalysis.clone(
+                                      muonSrc = cms.InputTag('selectedPatMuonsPFlow'),
+                                      electronSrc = cms.InputTag('selectedPatElectronsPFlow'),
+                                      metSrc = cms.InputTag('patMETsPFlow'),
+                                      jetSrc = cms.InputTag('selectedPatJetsPFlow'),
+                                      jetClonesSrc = cms.InputTag('goodPFJets'),
+                                      jetPtMin = cms.double(25.0),
+                                      minJets = cms.int32(1),
+                                      useJetClones = cms.bool(False)
+                                      )
+                                  
+                                  )
+
+process.caloShyftAna = cms.EDAnalyzer('EDSHyFT',
+                                  shyftAnalysis = inputShyftAnalysis.clone(
+                                      jetClonesSrc = cms.InputTag('goodCaloJets'),
+                                      jetPtMin = cms.double(30.0),
+                                      minJets = cms.int32(1),
+                                      useJetClones = cms.bool(False)
+                                      )
+                                  
+                                  )
 
 
 process.p = cms.Path(
-    process.shyftAna
+    process.pfShyftAna*process.caloShyftAna
     )
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
