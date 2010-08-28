fb5d9f3ae4a895d356f63857e958b0ef8fce9ada
srappocc <>
2010-08-28 04:01:00 +0000

    Updating shyft analysis to take advantage of a few speed improvements

--- SHyFT/bin/shyftAnalysis_cfg.py
+++ SHyFT/bin/shyftAnalysis_cfg.py
@@ -1,26 +1,44 @@
 import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("FWLitePlots")
+import string
+import sys
+
+filesin = sys.argv[2]
+outfile = sys.argv[3]
+
+#prepend = 'dcap://'
+prepend = ''
+
+print 'Input files from ' + filesin
+print 'Output file = ' + outfile
+
+infilenames = cms.vstring()
+txtfile = open(filesin)
+for line in txtfile.readlines():
+    infilenames.append( prepend + string.replace(line,'\n',''))
+
+print infilenames
 
 
 from Analysis.SHyFT.shyftAnalysis_cfi import shyftAnalysis as inputShyftAnalysis
 
 process.shyftAnalysis = inputShyftAnalysis.clone(
-#    muonSrc = cms.InputTag('selectedPatMuonsPFlow'),
-#    electronSrc = cms.InputTag('selectedPatElectronsPFlow'),
-#    metSrc = cms.InputTag('patMETsPFlow'),
-#    jetSrc = cms.InputTag('selectedPatJetsPFlow'),
-    jetPtMin = cms.double(30.0),
-    minJets = cms.int32(5)
+    muonSrc = cms.InputTag('selectedPatMuonsPFlow'),
+    electronSrc = cms.InputTag('selectedPatElectronsPFlow'),
+    metSrc = cms.InputTag('patMETsPFlow'),
+    jetSrc = cms.InputTag('selectedPatJetsPFlow'),
+    jetClonesSrc = cms.InputTag('goodPFJets'),
+    jetPtMin = cms.double(20.0),
+    minJets = cms.int32(5),
+    useJetClones = cms.bool(False)
     )
 
 process.inputs = cms.PSet (
-    fileNames = cms.vstring(
-        'shyft_382_pat.root'
-        ),
-        maxEvents = cms.int32(-1)
+    fileNames = infilenames,
+    maxEvents = cms.int32(1000)
 )
 
 process.outputs = cms.PSet (
-    outputName = cms.string('shyftPlotsStd.root')
+    outputName = cms.string(outfile)
 )
