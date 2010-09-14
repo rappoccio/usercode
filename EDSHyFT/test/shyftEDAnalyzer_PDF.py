ffc60307b550fd64371d0f2e611dca5c82af1b6a
srappocc <>
2010-09-14 01:41:11 +0000

    Add files to run over PDF up and down

--- EDSHyFT/test/shyftEDAnalyzer_PDF.py
+++ EDSHyFT/test/shyftEDAnalyzer_PDF.py
@@ -25,11 +25,6 @@ options.register ('useFlavorHistory',
                   VarParsing.varType.int,
                   "Flavor History Mode")
 
-options.register ('doMC',
-                  0,
-                  VarParsing.multiplicity.singleton,
-                  VarParsing.varType.int,
-                  "Use MC truth")
 
 options.register('sampleNameInput',
                  'top',
@@ -47,14 +42,6 @@ if options.useFlavorHistory > 0 :
     useFlavorHistory = True
 else :
     useFlavorHistory = False
-
-if options.doMC > 0 :
-    inputDoMC = True
-else :
-    inputDoMC = False
-    # get JSON file correctly parced
-    JSONfile = 'Cert_132440-144114_7TeV_StreamExpress_Collisions10_JSON.txt'
-    myList = LumiList.LumiList (filename = JSONfile).getCMSSWString().split(',')
 
 
 inputSampleName = options.sampleNameInput
@@ -74,65 +61,51 @@ else :
                                     )
                                 )
 
-if inputDoMC == False :
-    process.source.lumisToProcess = cms.untracked.VLuminosityBlockRange( myList )
-
 ## Maximal Number of Events
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
 from Analysis.SHyFT.shyftAnalysis_cfi import shyftAnalysis as inputShyftAnalysis
+
 
 process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("shyftStudies.root")
                                    )
 
 
-process.pfShyftAna = cms.EDAnalyzer('EDSHyFT',
-                                    shyftAnalysis = inputShyftAnalysis.clone(
-                                        muonSrc = cms.InputTag('selectedPatMuonsPFlow'),
-                                        electronSrc = cms.InputTag('selectedPatElectronsPFlow'),
-                                        metSrc = cms.InputTag('patMETsPFlow'),
-                                        jetSrc = cms.InputTag('selectedPatJetsPFlow'),
-                                        jetPtMin = cms.double(25.0),
-                                        minJets = cms.int32(1),
-                                        heavyFlavour = cms.bool( useFlavorHistory ),
-                                        doMC = cms.bool( inputDoMC),
-                                        sampleName = cms.string(inputSampleName),
-                                        identifier = cms.string('PF')
-                                        )                                    
-                                    )
-
-process.jptShyftAna = cms.EDAnalyzer('EDSHyFT',
-                                     shyftAnalysis = inputShyftAnalysis.clone(
-                                         metSrc = cms.InputTag('patMETsTC'),
-                                         jetSrc = cms.InputTag('selectedPatJetsAK5JPT'),                                         
-                                         jetPtMin = cms.double(30.0),
-                                         minJets = cms.int32(1),
-                                         heavyFlavour = cms.bool( useFlavorHistory ),
-                                         doMC = cms.bool( inputDoMC),
-                                         sampleName = cms.string(inputSampleName),
-                                         identifier = cms.string('JPT')
-                                        )
-                                     
-                                     )
-
 process.caloShyftAna = cms.EDAnalyzer('EDSHyFT',
                                       shyftAnalysis = inputShyftAnalysis.clone(
                                           jetPtMin = cms.double(30.0),
                                           minJets = cms.int32(1),
                                           heavyFlavour = cms.bool( useFlavorHistory ),
-                                          doMC = cms.bool( inputDoMC),
+                                          doMC = cms.bool( True),
                                           sampleName = cms.string(inputSampleName),
                                           btaggerString = cms.string('simpleSecondaryVertexBJetTags'),
                                           identifier = cms.string('CALO')
                                           )                                      
                                       )
 
+process.caloShyftAnaUP = process.caloShyftAna.clone(
+    shyftAnalysis = process.caloShyftAna.shyftAnalysis.clone(
+        reweightPDF = cms.bool(True),
+        pdfToUse = cms.string('cteq61.LHgrid'),
+        pdfVariation =cms.int32( 1 ),
+        identifier=cms.string('CALOUP')
+        )
+    )
+
+process.caloShyftAnaDOWN = process.caloShyftAna.clone(
+    shyftAnalysis = process.caloShyftAna.shyftAnalysis.clone(
+        reweightPDF = cms.bool(True),
+        pdfToUse = cms.string('cteq61.LHgrid'),
+        pdfVariation =cms.int32( -1 ),
+        identifier=cms.string('CALODOWN')
+        )
+    )
 
 process.p = cms.Path(
-    process.pfShyftAna
-#    process.pfShyftAna*process.jptShyftAna*process.caloShyftAna
-#    process.jptShyftAna
+    process.caloShyftAna*
+    process.caloShyftAnaUP*
+    process.caloShyftAnaDOWN
     )
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 10000
