61f32869cb881af901d7a0b6a302c28ed1a61105
guofan <>
2010-09-20 18:14:32 +0000

    Add medium mass

--- EDBoostedTopAnalysis/plugins/EDBoostedTopAnalysis.cc
+++ EDBoostedTopAnalysis/plugins/EDBoostedTopAnalysis.cc
@@ -7,13 +7,14 @@
 #include "PhysicsTools/UtilAlgos/interface/FWLiteAnalyzerWrapper.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-
+#include "Analysis/BoostedTopAnalysis/interface/WPlusBJetAnalysis.h"
 
 typedef edm::FWLiteAnalyzerWrapper<HadronicAnalysis> EDHadronicAnalysis;
 typedef edm::FWLiteAnalyzerWrapper<SemileptonicAnalysis> EDSemileptonicAnalysis;
-
+typedef edm::FWLiteAnalyzerWrapper<WPlusBJetAnalysis>   EDWPlusBJetAnalysis;
 
 DEFINE_FWK_MODULE(EDHadronicAnalysis);
 DEFINE_FWK_MODULE(EDSemileptonicAnalysis);
+DEFINE_FWK_MODULE(EDWPlusBJetAnalysis);
 
 #endif
