ccaca935519c7fecb3a652d65fc53f4e1ed4447e
jdolen <>
2011-03-08 04:35:11 +0000

    Add new combined code

--- EDBoostedTopAnalysis/plugins/EDBoostedTopAnalysis.cc
+++ EDBoostedTopAnalysis/plugins/EDBoostedTopAnalysis.cc
@@ -9,6 +9,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "Analysis/BoostedTopAnalysis/interface/WPlusBJetAnalysis.h"
+#include "Analysis/BoostedTopAnalysis/interface/CombinedQCDEstimation.h"
 #include "Analysis/BoostedTopAnalysis/interface/Type22QCDEstimation.h"
 #include "Analysis/BoostedTopAnalysis/interface/MistagMaker.h"
 
@@ -16,12 +17,14 @@
 typedef edm::AnalyzerWrapper<HadronicAnalysis> EDHadronicAnalysis;
 typedef edm::AnalyzerWrapper<SemileptonicAnalysis> EDSemileptonicAnalysis;
 typedef edm::AnalyzerWrapper<WPlusBJetAnalysis>   EDWPlusBJetAnalysis;
+typedef edm::AnalyzerWrapper<CombinedQCDEstimation>  EDCombinedQCDEstimation;
 typedef edm::AnalyzerWrapper<Type22QCDEstimation>  EDType22QCDEstimation;
 typedef edm::AnalyzerWrapper<MistagMaker>       EDMistagMaker;
 
 DEFINE_FWK_MODULE(EDHadronicAnalysis);
 DEFINE_FWK_MODULE(EDSemileptonicAnalysis);
 DEFINE_FWK_MODULE(EDWPlusBJetAnalysis);
+DEFINE_FWK_MODULE(EDCombinedQCDEstimation);
 DEFINE_FWK_MODULE(EDType22QCDEstimation);
 DEFINE_FWK_MODULE(EDMistagMaker);
 
