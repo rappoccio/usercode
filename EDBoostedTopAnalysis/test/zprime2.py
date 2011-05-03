b74a84022edf8e24b6914c6aabf8bb88216ccb05
srappocc <>
2011-05-03 18:23:25 +0000

    Adding 40 pb-1 first step

--- EDBoostedTopAnalysis/test/zprime2.py
+++ EDBoostedTopAnalysis/test/zprime2.py
@@ -8,6 +8,62 @@ process.load("FWCore.MessageLogger.MessageLogger_cfi")
 ## Options and Output Report
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
 
+###############################
+####### Parameters ############
+###############################
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('python')
+
+options.register ('useTrigger',
+                  0,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "Select events by trigger path")
+
+
+options.register ('jetPD',
+                  0,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "use triggers from the Jet PD (1), ignore them (0), or veto them (-1), if useTrigger == 1")
+
+
+options.register ('htPD',
+                  0,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "use triggers from the HT PD (1), ignore them (0), or veto them (-1), if useTrigger == 1")
+
+
+
+options.parseArguments()
+
+print options
+
+###############################
+########## Trigger ############
+###############################
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+if options.useTrigger :
+    process.jetTrig = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = ["HLT_Jet370_*"])
+    process.htTrig  = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = ["HLT_HT500_*"])
+
+    process.jetTrig.throw= cms.bool(False)
+    process.htTrig.throw= cms.bool(False)
+
+    if options.jetPD > 0 :
+        process.trigs = cms.Sequence( process.jetTrig )
+    elif options.jetPD < 0 and options.htPD > 0 :
+        process.trigs = cms.Sequence( ~process.jetTrig *
+                                      process.htTrig )
+    elif options.htPD > 0 :
+        process.trigs = cms.Sequence( process.htTrig )
+else :
+    process.alltrig = hltHighLevel.clone(TriggerResultsTag = "TriggerResults::HLT", HLTPaths = ["*"])
+    process.trigs = cms.Sequence( process.alltrig )
+
+
+
 ## Source
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
@@ -32,32 +88,24 @@ process.TFileService = cms.Service("TFileService",
                                    fileName = cms.string("jetPD.root")
                                    )
 
-process.type22QCDAna15 = cms.EDAnalyzer('EDCombinedQCDEstimation',
-                                  Type11QCDEstimationParams,
-                                  Type22QCDEstimationParams
-                                     )
+process.cascadingQCDAna15 = cms.EDAnalyzer('EDCombinedQCDEstimation',
+                                           Type11QCDEstimationParams,
+                                           Type22QCDEstimationParams
+                                           )
 
 #process.type22QCDAna10 = process.type22QCDAna15.clone( Probability = cms.double(0.10) )
 #process.type22QCDAna5 = process.type22QCDAna15.clone( Probability = cms.double(0.05) )
 #process.type22QCDAna33 = process.type22QCDAna15.clone( Probability = cms.double(0.33) )
 
 process.p = cms.Path(
-  process.type22QCDAna15#*process.type22QCDAna10*process.type22QCDAna5*process.type22QCDAna33
+    process.trigs*
+    process.cascadingQCDAna15
     )
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
 process.RandomNumberGeneratorService = cms.Service("RandomNumberGeneratorService",
-                      type22QCDAna15 = cms.PSet(
-                      initialSeed = cms.untracked.uint32(81)
-                      ),
-                      type22QCDAna10 = cms.PSet(
-                      initialSeed = cms.untracked.uint32(81)
-                      ),
-                      type22QCDAna5 = cms.PSet(
-                      initialSeed = cms.untracked.uint32(81)
-                      ),
-                      type22QCDAna33 = cms.PSet(
+                      cascadingQCDAna15 = cms.PSet(
                       initialSeed = cms.untracked.uint32(81)
                       )
 
