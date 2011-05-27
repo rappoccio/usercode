f2aea821c8ced574cd657bebd09da1eb7897fa7c
srappocc <>
2011-05-27 12:25:00 +0000

    Adding a super-slim TTBSM-tuple producer

--- TTBSMPatTuples/plugins/TTBSMProducer.cc
+++ TTBSMPatTuples/plugins/TTBSMProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  "Salvatore Rappoccio"
 //         Created:  Mon Jan 17 21:44:07 CST 2011
-// $Id$
+// $Id: TTBSMProducer.cc,v 1.1 2011/01/19 15:52:05 srappocc Exp $
 //
 //
 
@@ -52,10 +52,9 @@ class TTBSMProducer : public edm::EDFilter {
       // ----------member data ---------------------------
   edm::InputTag             wTagSrc_; 
   edm::InputTag             topTagSrc_;
-  PFJetIDSelectionFunctor   pfJetId_;
+  std::string               topTagName_;
   CATopTagFunctor           topTagFunctor_;
   BoostedTopWTagFunctor     wTagFunctor_;
-  std::string               topTagName_;
 };
 
 //
@@ -73,14 +72,20 @@ class TTBSMProducer : public edm::EDFilter {
 TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   wTagSrc_      (iConfig.getParameter<edm::InputTag>("wTagSrc") ),
   topTagSrc_    (iConfig.getParameter<edm::InputTag>("topTagSrc") ),
-  pfJetId_      (iConfig.getParameter<edm::ParameterSet>("pfJetIdParams") ),
-  topTagFunctor_(iConfig.getParameter<edm::ParameterSet>("topTagParams") ),
-  wTagFunctor_  (iConfig.getParameter<edm::ParameterSet>("wTagParams") ),
-  topTagName_   (iConfig.getParameter<edm::ParameterSet>("topTagParams").getParameter<std::string>("tagName") )
+  topTagName_   (iConfig.getParameter<edm::ParameterSet>("topTagParams").getParameter<std::string>("tagName") ),
+  topTagFunctor_(iConfig.getParameter<edm::ParameterSet>("topTagParams")),
+  wTagFunctor_  (iConfig.getParameter<edm::ParameterSet>("wTagParams"))
 {
   //register your products
-  produces<std::vector<pat::Jet> > ("wTag");
-  produces<std::vector<pat::Jet> > ("topTag");  
+  produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4");
+  produces<std::vector<reco::Candidate::PolarLorentzVector> > ("topTagP4");  
+  produces<std::vector<double> > ("wTagBDisc");
+  produces<std::vector<double> > ("wTagMu");
+  produces<std::vector<int> >    ("wTagPass");
+  produces<std::vector<double> > ("topTagMinMass");
+  produces<std::vector<double> > ("topTagTopMass");
+  produces<std::vector<double> > ("topTagNSubjets");
+  produces<std::vector<int> >    ("topTagPass");
 }
 
 
@@ -98,8 +103,17 @@ bool
 TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
-  std::auto_ptr<std::vector<pat::Jet> >  selectedWTag( new std::vector<pat::Jet> );
-  std::auto_ptr<std::vector<pat::Jet> >  selectedTopTag( new std::vector<pat::Jet> );
+  typedef std::vector<reco::Candidate::PolarLorentzVector> p4_vector;
+
+  std::auto_ptr<p4_vector> wTagP4( new p4_vector() );
+  std::auto_ptr<p4_vector> topTagP4( new p4_vector() );
+  std::auto_ptr<std::vector<double> > wTagBDisc ( new std::vector<double>() );
+  std::auto_ptr<std::vector<double> > wTagMu ( new std::vector<double>() );
+  std::auto_ptr<std::vector<int> >    wTagPass ( new std::vector<int>() );
+  std::auto_ptr<std::vector<double> > topTagMinMass ( new std::vector<double>() );
+  std::auto_ptr<std::vector<double> > topTagTopMass ( new std::vector<double>() );
+  std::auto_ptr<std::vector<double> > topTagNSubjets ( new std::vector<double>() );
+  std::auto_ptr<std::vector<int> >    topTagPass ( new std::vector<int>() );
 
   edm::Handle<std::vector<pat::Jet> > h_wTag;
   edm::Handle<std::vector<pat::Jet> > h_topTag;
@@ -107,41 +121,42 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.getByLabel( wTagSrc_, h_wTag );
   iEvent.getByLabel( topTagSrc_, h_topTag );
 
-  pat::strbitset jetIdRet = pfJetId_.getBitTemplate();
   pat::strbitset wTagRet = wTagFunctor_.getBitTemplate();
   pat::strbitset topTagRet = topTagFunctor_.getBitTemplate();
   for ( std::vector<pat::Jet>::const_iterator jetBegin = h_wTag->begin(),
 	  jetEnd = h_wTag->end(), ijet = jetBegin; ijet != jetEnd; ++ijet ) {
-    bool passJetId = pfJetId_( *ijet, jetIdRet );
-    if ( passJetId ) {
-      selectedWTag->push_back( *ijet );
-      double y = -1.0, mu = -1.0, dR = -1.0;
-      pat::subjetHelper( selectedWTag->back(), y, mu, dR );
-      selectedWTag->back().addUserFloat( "y", y );
-      selectedWTag->back().addUserFloat( "mu", mu );
-      selectedWTag->back().addUserFloat( "dR", dR );
-      bool passedWTag = wTagFunctor_(*ijet, wTagRet);
-      selectedWTag->back().addUserInt( "wTagPass", (int)passedWTag );
-    }
+    wTagP4->push_back( ijet->polarP4() );
+    bool passedWTag = wTagFunctor_(*ijet, wTagRet);
+    double y = -1.0, mu = -1.0, dR = -1.0;
+    pat::subjetHelper( *ijet, y, mu, dR );
+    wTagMu->push_back( mu );
+    wTagPass->push_back( passedWTag );
+    wTagBDisc->push_back( ijet->bDiscriminator("trackCountingHighEffBJetTags") );
   }
 
   for ( std::vector<pat::Jet>::const_iterator jetBegin = h_topTag->begin(),
 	  jetEnd = h_topTag->end(), ijet = jetBegin; ijet != jetEnd; ++ijet ) {
-    bool passJetId = pfJetId_( *ijet, jetIdRet );
-    if ( passJetId ) {
-      selectedTopTag->push_back( *ijet );
-      const reco::CATopJetTagInfo * catopTag = 
-	dynamic_cast<reco::CATopJetTagInfo const *>(selectedTopTag->back().tagInfo(topTagName_));
-      selectedTopTag->back().addUserFloat( "minMass", catopTag->properties().minMass );
-      selectedTopTag->back().addUserFloat( "topMass", catopTag->properties().topMass );
-      selectedTopTag->back().addUserFloat( "nSubjets", ijet->numberOfDaughters() );
-      bool passedTopTag = topTagFunctor_( *ijet, topTagRet );
-      selectedTopTag->back().addUserInt( "topTagPass", (int)passedTopTag );
-    }
+    const reco::CATopJetTagInfo * catopTag = 
+      dynamic_cast<reco::CATopJetTagInfo const *>(ijet->tagInfo(topTagName_));
+    bool passedTopTag = topTagFunctor_( *ijet, topTagRet );
+    topTagP4->push_back( ijet->polarP4() );
+    topTagPass->push_back( passedTopTag );
+    topTagMinMass->push_back( catopTag->properties().minMass );
+    topTagTopMass->push_back( catopTag->properties().topMass );
+    topTagNSubjets->push_back( ijet->numberOfDaughters() );
+
   }
 
-  iEvent.put( selectedWTag, "wTag");  
-  iEvent.put( selectedTopTag, "topTag");   
+  iEvent.put(wTagP4        ,"wTagP4");
+  iEvent.put(topTagP4      ,"topTagP4");  
+  iEvent.put(wTagBDisc     ,"wTagBDisc");
+  iEvent.put(wTagMu        ,"wTagMu");
+  iEvent.put(wTagPass      ,"wTagPass");
+  iEvent.put(topTagMinMass ,"topTagMinMass");
+  iEvent.put(topTagTopMass ,"topTagTopMass");
+  iEvent.put(topTagNSubjets,"topTagNSubjets");
+  iEvent.put(topTagPass    ,"topTagPass");    
+
 
   return true;
 }
---
+++ TTBSMPatTuples/test/ttbsmTupleProducer_cfg.py
@@ -0,0 +1,60 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TTBSM")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+
+process.source = cms.Source("PoolSource",
+    # replace 'myfile.root' with the source file you want to use
+    fileNames = cms.untracked.vstring(
+'dcap:///pnfs/cms/WAX/11/store/user/lpctlbsm/srappocc/Jet/ttbsm_v6_Run2011-May10ReReco/7e150b77ce1bf887c7a9afa63377fb1c/ttbsm_42x_data_200_0_pIF.root',
+    )
+)
+
+from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
+from Analysis.BoostedTopAnalysis.CATopTagParams_cfi import caTopTagParams
+from Analysis.BoostedTopAnalysis.BoostedTopWTagParams_cfi import boostedTopWTagParams
+
+
+mytrigs = ['HLT_Jet370*']
+from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+process.hltSelection = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs)
+process.hltSelection.throw = False
+
+
+process.ttbsmAna = cms.EDFilter('TTBSMProducer',
+                                wTagSrc = cms.InputTag('goodPatJetsCA8PrunedPF'),
+                                topTagSrc = cms.InputTag('goodPatJetsCATopTagPF'),
+                                topTagParams = caTopTagParams.clone(
+                                    tagName = cms.string('CATop')
+                                    ),
+                                wTagParams = boostedTopWTagParams.clone(
+                                    yCut = cms.double(0.0)
+                                    )
+)
+
+
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+process.p = cms.Path(
+    process.hltSelection*
+    process.ttbsmAna
+    )
+
+
+process.out = cms.OutputModule("PoolOutputModule",
+                               fileName = cms.untracked.string("ttbsm_ultraslim.root"),
+                               SelectEvents   = cms.untracked.PSet( SelectEvents = cms.vstring('p') ),
+                               outputCommands = cms.untracked.vstring('drop *',
+                                                                      'keep *_ttbsmAna_*_*'
+                                                                      #, 'keep *_goodPatJetsCA8PrunedPF_*_*'
+                                                                      #, 'keep *_goodPatJetsCATopTagPF_*_*'
+                                                                      #, 'keep recoPFJets_*_*_*'
+                                                                      ) 
+                               )
+process.outpath = cms.EndPath(process.out)
+
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+process.out.dropMetaData = cms.untracked.string("DROPPED")
