cbf0cbbec97b07eeeb50fe97fa0984c533d7a6d1
skhalil <>
2011-06-13 01:58:46 +0000

    test scripts to be run on ttbsm PAT tuples

--- SHyFT/bin/shyftAnalysis_ele_cfg.py
+++ SHyFT/bin/shyftAnalysis_ele_cfg.py
@@ -6,6 +6,8 @@ import sys
 
 filesin = sys.argv[2]
 outfile = sys.argv[3]
+#outfile = 'wjets_ttbsm.root'
+#filesin = 'wjets_ttbsm.txt'
 
 #prepend = 'dcap:///pnfs/cms/WAX/11'
 prepend = ''
@@ -16,10 +18,9 @@ print 'Output file = ' + outfile
 infilenames = cms.vstring()
 txtfile = open(filesin)
 for line in txtfile.readlines():
-    infilenames.append( prepend + string.replace(line,'\n',''))
+   infilenames.append( prepend + string.replace(line,'\n',''))
 
 print infilenames
-
 
 from Analysis.SHyFT.shyftAnalysis_cfi import shyftAnalysis as inputShyftAnalysis
 
@@ -27,8 +28,9 @@ process.shyftAnalysis = inputShyftAnalysis.clone(
     muonSrc = cms.InputTag('selectedPatMuonsPFlow'),
     electronSrc = cms.InputTag('selectedPatElectronsPFlow'),
     metSrc = cms.InputTag('patMETsPFlow'),
-    #jetSrc = cms.InputTag('goodPatJetsPFlow'),
-    jetSrc = cms.InputTag('selectedPatJetsPFlow'),
+    jetSrc = cms.InputTag('goodPatJetsPFlow'),
+    #jetSrc = cms.InputTag('selectedPatJetsPFlow'),
+    pvSrc   = cms.InputTag('goodOfflinePrimaryVertices'),
     jetPtMin = cms.double(25.0),
     minJets = cms.int32(5),
     eRelIso = cms.double(0.1),
@@ -39,9 +41,56 @@ process.shyftAnalysis = inputShyftAnalysis.clone(
     wMTMax = cms.double(10000.),
     cutsToIgnore=cms.vstring('Trigger'),
 
-    #metMax = cms.double(25.),
+    pvSelector = cms.PSet(
+    pvSrc = cms.InputTag('goodOfflinePrimaryVertices'),
+    minNdof = cms.double(4.0),
+    maxZ = cms.double(15.0),
+    maxRho = cms.double(2.0),
+    ),
 
-   #***********Uncomment if want to test on WJets, ZJets, Vqq, WcJets******************
+    muonIdTight = cms.PSet(
+    version = cms.string('FALL10'),
+    Chi2 = cms.double(10.0),
+    D0 = cms.double(0.02),
+    ED0 = cms.double(999.0),
+    SD0 = cms.double(999.0),
+    NHits = cms.int32(11),
+    NValMuHits = cms.int32(0),
+    ECalVeto = cms.double(999.0),
+    HCalVeto = cms.double(999.0),
+    RelIso = cms.double(0.05),
+    LepZ = cms.double(1.0),
+    nPixelHits = cms.int32(1),
+    nMatchedStations=cms.int32(1),
+    cutsToIgnore = cms.vstring('ED0', 'SD0', 'ECalVeto', 'HCalVeto'),
+    RecalcFromBeamSpot = cms.bool(False),
+    beamLineSrc = cms.InputTag("offlineBeamSpot"),
+    pvSrc = cms.InputTag("goodOfflinePrimaryVertices"),
+    ),
+    
+    muonIdLoose = cms.PSet(
+    version = cms.string('FALL10'),
+    Chi2 = cms.double(999.0),
+    D0 = cms.double(999.0),
+    ED0 = cms.double(999.0),
+    SD0 = cms.double(999.0),
+    NHits = cms.int32(-1),
+    NValMuHits = cms.int32(-1),
+    ECalVeto = cms.double(999.0),
+    HCalVeto = cms.double(999.0),
+    RelIso = cms.double(0.2),
+    LepZ = cms.double(1.0),
+    nPixelHits = cms.int32(1),
+    nMatchedStations=cms.int32(1),        
+    cutsToIgnore = cms.vstring('Chi2', 'D0', 'ED0', 'SD0', 'NHits','NValMuHits','ECalVeto','HCalVeto','LepZ','nPixelHits','nMatchedStations'),
+    RecalcFromBeamSpot = cms.bool(False),
+    beamLineSrc = cms.InputTag("offlineBeamSpot"),
+    pvSrc = cms.InputTag("goodOfflinePrimaryVertices"),
+    ),
+    
+    #metMax = cms.double(25.),
+    
+    #***********Uncomment if want to test on WJets, ZJets, Vqq, WcJets******************
     #sampleName="Wjets",
     #sampleName = "Top",
     #doMC = True,
@@ -59,7 +108,7 @@ process.shyftAnalysis = inputShyftAnalysis.clone(
 
 process.inputs = cms.PSet (
     fileNames = infilenames,
-    maxEvents = cms.int32(1000)
+    maxEvents = cms.int32(-1)
 )
 
 
