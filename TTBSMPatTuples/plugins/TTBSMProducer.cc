5e63121b36f38394737544ce6b7ee0a39a1bd161
guofan <>
2011-06-28 13:52:26 +0000

    Document all

--- TTBSMPatTuples/plugins/TTBSMProducer.cc
+++ TTBSMPatTuples/plugins/TTBSMProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  "Salvatore Rappoccio"
 //         Created:  Mon Jan 17 21:44:07 CST 2011
-// $Id: TTBSMProducer.cc,v 1.2 2011/05/27 12:25:00 srappocc Exp $
+// $Id: TTBSMProducer.cc,v 1.3 2011/05/29 05:15:58 guofan Exp $
 //
 //
 
@@ -35,9 +35,14 @@
 #include "AnalysisDataFormats/TopObjects/interface/CATopJetTagInfo.h"
 #include "Analysis/BoostedTopAnalysis/interface/CATopTagFunctor.h"
 #include "Analysis/BoostedTopAnalysis/interface/BoostedTopWTagFunctor.h"
+//#include "DataFormats/PatCandidates/interface/TriggerEvent.h"
+//#include "DataFormats/PatCandidates/interface/TriggerPath.h"
+#include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+
 //
 // class declaration
 //
+using namespace std;
 
 class TTBSMProducer : public edm::EDFilter {
    public:
@@ -52,6 +57,8 @@ class TTBSMProducer : public edm::EDFilter {
       // ----------member data ---------------------------
   edm::InputTag             wTagSrc_; 
   edm::InputTag             topTagSrc_;
+  edm::InputTag             trigSrc_;
+  std::vector<std::string>  trigs_;
   std::string               topTagName_;
   CATopTagFunctor           topTagFunctor_;
   BoostedTopWTagFunctor     wTagFunctor_;
@@ -72,6 +79,8 @@ class TTBSMProducer : public edm::EDFilter {
 TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   wTagSrc_      (iConfig.getParameter<edm::InputTag>("wTagSrc") ),
   topTagSrc_    (iConfig.getParameter<edm::InputTag>("topTagSrc") ),
+  trigSrc_      (iConfig.getParameter<edm::InputTag>("trigSrc") ),
+  trigs_        (iConfig.getParameter<std::vector<std::string> > ("trigs") ),
   topTagName_   (iConfig.getParameter<edm::ParameterSet>("topTagParams").getParameter<std::string>("tagName") ),
   topTagFunctor_(iConfig.getParameter<edm::ParameterSet>("topTagParams")),
   wTagFunctor_  (iConfig.getParameter<edm::ParameterSet>("wTagParams"))
@@ -79,6 +88,7 @@ TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   //register your products
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4");
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("topTagP4");  
+  produces<std::vector<double> > ("topTagBDisc");
   produces<std::vector<double> > ("wTagBDisc");
   produces<std::vector<double> > ("wTagMu");
   produces<std::vector<int> >    ("wTagPass");
@@ -86,6 +96,11 @@ TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   produces<std::vector<double> > ("topTagTopMass");
   produces<std::vector<double> > ("topTagNSubjets");
   produces<std::vector<int> >    ("topTagPass");
+  produces<std::vector<int> >    ("prescales");
+  produces<std::vector<int> >    ("trigs");
+  produces<std::vector<std::string> > ("trigNames");
+  produces<double> ("rho");
+  produces<double> ("weight");
 
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4Hemis0");
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4Hemis1");
@@ -123,19 +138,28 @@ bool
 TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
 
+  //cout<<"TTBSMProducer per Event!"<<endl;
+
   typedef std::vector<reco::Candidate::PolarLorentzVector> p4_vector;
   typedef reco::Candidate::PolarLorentzVector LorentzV;
+  //cout<<"Start!"<<endl;
 
   std::auto_ptr<p4_vector> wTagP4( new p4_vector() );
   std::auto_ptr<p4_vector> topTagP4( new p4_vector() );
   std::auto_ptr<std::vector<double> > wTagBDisc ( new std::vector<double>() );
+  std::auto_ptr<std::vector<double> > topTagBDisc ( new std::vector<double>() );
   std::auto_ptr<std::vector<double> > wTagMu ( new std::vector<double>() );
   std::auto_ptr<std::vector<int> >    wTagPass ( new std::vector<int>() );
   std::auto_ptr<std::vector<double> > topTagMinMass ( new std::vector<double>() );
   std::auto_ptr<std::vector<double> > topTagTopMass ( new std::vector<double>() );
   std::auto_ptr<std::vector<double> > topTagNSubjets ( new std::vector<double>() );
   std::auto_ptr<std::vector<int> >    topTagPass ( new std::vector<int>() );
-  
+  std::auto_ptr<std::vector<int> >    prescales ( new std::vector<int>() );
+  std::auto_ptr<std::vector<int> >    trigs ( new std::vector<int>() );
+  std::auto_ptr<std::vector<std::string> >    trigNames ( new std::vector<std::string>() );
+  std::auto_ptr<double>               rho( new double(-1.0) );
+  std::auto_ptr<double>               weight( new double(1.0) );
+
   //The duplicate quantities by hemisphere
   std::auto_ptr<p4_vector> topTagP4Hemis0 ( new p4_vector() );
   std::auto_ptr<p4_vector> topTagP4Hemis1 ( new p4_vector() );
@@ -156,17 +180,16 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::auto_ptr<std::vector<int> > topTagPassHemis0  ( new std::vector<int>() );
   std::auto_ptr<std::vector<int> > topTagPassHemis1  ( new std::vector<int>() );
 
-
-
-
-
-
+  //cout<<"Finish auto_ptr"<<endl;
   edm::Handle<std::vector<pat::Jet> > h_wTag;
   edm::Handle<std::vector<pat::Jet> > h_topTag;
+  //edm::Handle<pat::TriggerEvent>      h_trig;
 
+  //cout<<"Finish define handle"<<endl;
   iEvent.getByLabel( wTagSrc_, h_wTag );
   iEvent.getByLabel( topTagSrc_, h_topTag );
 
+  //cout<<"Finish get content"<<endl;
   pat::strbitset wTagRet = wTagFunctor_.getBitTemplate();
   pat::strbitset topTagRet = topTagFunctor_.getBitTemplate();
   for ( std::vector<pat::Jet>::const_iterator jetBegin = h_wTag->begin(),
@@ -190,6 +213,7 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
     topTagMinMass->push_back( catopTag->properties().minMass );
     topTagTopMass->push_back( catopTag->properties().topMass );
     topTagNSubjets->push_back( ijet->numberOfDaughters() );
+    topTagBDisc->push_back( ijet->bDiscriminator("trackCountingHighEffBJetTags") );
 
   }
 
@@ -253,16 +277,53 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
   }
 
+  //cout<<"Finish Jets!"<<endl;
+
+  edm::Handle<double>        caTopJetRho;
+  iEvent.getByLabel( edm::InputTag("kt6PFJetsPFlow", "rho"),    caTopJetRho );
+  *rho = *caTopJetRho ;
+  edm::Handle<GenEventInfoProduct>    genEvt;
+  iEvent.getByLabel( edm::InputTag("generator"),  genEvt );
+  if( genEvt.isValid() )  {
+    *weight = genEvt->weight() ;
+  }
+  //cout<<"Finish all info"<<endl;
+  //std::cout<<*rho<<std::endl;
+/*
+  // For real data, get trigger path
+  if ( iEvent.isRealData() ) {
+    iEvent.getByLabel( trigSrc_,h_trig );
+    for ( std::vector<std::string>::const_iterator itrigBegin = trigs_.begin(),
+            itrigEnd = trigs_.end(), itrig = itrigBegin;
+          itrig != itrigEnd; ++itrig ) {
+      if ( h_trig->wasRun() && h_trig->wasAccept() && h_trig->paths() > 0 ) {
+        int indexPath = h_trig->indexPath( *itrig );
+        if ( indexPath > 0 ) {
+          pat::TriggerPath const * path = h_trig->path( *itrig );
+          if ( path != 0 && path->wasRun() && path->wasAccept() ) {
+            trigs->push_back( path->index() );
+            prescales->push_back( path->prescale() );
+            trigNames->push_back( path->name() );
+          }
+        }
+      }
+    }
+  }
+*/
 
   iEvent.put(wTagP4        ,"wTagP4");
   iEvent.put(topTagP4      ,"topTagP4");  
   iEvent.put(wTagBDisc     ,"wTagBDisc");
+  iEvent.put(topTagBDisc   ,"topTagBDisc");
   iEvent.put(wTagMu        ,"wTagMu");
   iEvent.put(wTagPass      ,"wTagPass");
   iEvent.put(topTagMinMass ,"topTagMinMass");
   iEvent.put(topTagTopMass ,"topTagTopMass");
   iEvent.put(topTagNSubjets,"topTagNSubjets");
   iEvent.put(topTagPass    ,"topTagPass");    
+  iEvent.put(prescales     ,"prescales");
+  iEvent.put(trigs         ,"trigs");
+  iEvent.put(trigNames     ,"trigNames");
 
   iEvent.put(wTagP4Hemis0        ,"wTagP4Hemis0");
   iEvent.put(topTagP4Hemis0      ,"topTagP4Hemis0");
@@ -283,9 +344,10 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.put(topTagNSubjetsHemis1,"topTagNSubjetsHemis1");
   iEvent.put(topTagPassHemis1    ,"topTagPassHemis1");
   iEvent.put(jet3Hemis1,          "jet3Hemis1"  );
+  iEvent.put( rho,                "rho" );
+  iEvent.put( weight,             "weight");
 
-
-
+  //cout<<"Finish putting"<<endl;
   return true;
 }
 
--- TTBSMPatTuples/test/ttbsmTupleProducer_cfg.py
+++ TTBSMPatTuples/test/ttbsmTupleProducer_cfg.py
@@ -2,31 +2,102 @@ import FWCore.ParameterSet.Config as cms
 
 process = cms.Process("TTBSM")
 
+
+###############################
+####### Parameters ############
+###############################
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('python')
+
+
+
+
+options.register ('sample',
+                  'Jet',
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.string,
+                  "Sample to use.")
+
+
+
+
+options.parseArguments()
+
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10000) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 
-process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
-    fileNames = cms.untracked.vstring(
-'dcap:///pnfs/cms/WAX/11/store/user/lpctlbsm/srappocc/Jet/ttbsm_v6_Run2011-May10ReReco/7e150b77ce1bf887c7a9afa63377fb1c/ttbsm_42x_data_200_0_pIF.root',
-    )
-)
+
+
+## Geometry and Detector Conditions (needed for a few patTuple production steps)
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = 'GR_R_42_V12::All'
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
 
 from PhysicsTools.SelectorUtils.pfJetIDSelector_cfi import pfJetIDSelector
 from Analysis.BoostedTopAnalysis.CATopTagParams_cfi import caTopTagParams
 from Analysis.BoostedTopAnalysis.BoostedTopWTagParams_cfi import boostedTopWTagParams
 
 
-mytrigs = ['HLT_Jet370*']
-from HLTrigger.HLTfilters.hltHighLevel_cfi import *
-process.hltSelection = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs)
-process.hltSelection.throw = False
+if options.sample == 'Jet' :
+    process.source = cms.Source("PoolSource",
+                                # replace 'myfile.root' with the source file you want to use
+                                fileNames = cms.untracked.vstring(
+#'dcap:///pnfs/cms/WAX/11/store/user/lpctlbsm/srappocc/Jet/ttbsm_v6_Run2011-May10ReReco/7e150b77ce1bf887c7a9afa63377fb1c/ttbsm_42x_data_200_0_pIF.root',
+'dcap://cmsdca.fnal.gov:24136/pnfs/fnal.gov/usr/cms/WAX/11/store/user/lpctlbsm/srappocc/Jet/ttbsm_v8_Run2011-May10ReReco/0d3d9a54f3a29af186ad87df2a0c3ce1/ttbsm_42x_data_75_1_QBL.root'
+                                    )
+                                )
+
+    
+    mytrigs = [
+        #'HLT_Jet190_v1',
+        #'HLT_Jet190_v2',
+        #'HLT_Jet190_v3',
+        #'HLT_Jet240_v1',
+        #'HLT_Jet240_v2',
+        #'HLT_Jet240_v3',        
+        'HLT_Jet300_v1',
+        'HLT_Jet300_v2',
+        'HLT_Jet370_v1',
+        'HLT_Jet370_v2',
+        'HLT_Jet370_v3']
+    from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+    process.hltSelection1 = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs)
+    process.hltSelection1.throw = False
+    process.hltSelection = cms.Sequence( process.hltSelection1 )
+elif options.sample == 'HT' :
+
+    process.source = cms.Source("PoolSource",
+                                # replace 'myfile.root' with the source file you want to use
+                                fileNames = cms.untracked.vstring(
+'dcap:///pnfs/cms/WAX/11/store/user/lpctlbsm/guofan/HT/ttbsm_v5_Run2011A-May10ReReco-v1/66893cf1a11d0e3066e5097c4fdc37a6/ttbsm_42x_data_74_1_PRV.root',
+                                    )
+                                )
+    
+    mytrigs1 = ['HLT_Jet370*', "HLT_Jet300*"]
+    mytrigs2 = ['HLT_HT500*']
+    mytrigs = []
+    from HLTrigger.HLTfilters.hltHighLevel_cfi import *
+    process.hltSelection1 = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs1)
+    process.hltSelection1.throw = False
+    process.hltSelection2 = hltHighLevel.clone(TriggerResultsTag = 'TriggerResults::HLT', HLTPaths = mytrigs2)
+    process.hltSelection2.throw = False
+    process.hltSelection = cms.Sequence( ~process.hltSelection1 * process.hltSelection2 )
+
+    
+# run the trigger on the fly
+#process.load('PhysicsTools.PatAlgos.triggerLayer1.triggerProducer_cff')
 
 
 process.ttbsmAna = cms.EDFilter('TTBSMProducer',
                                 wTagSrc = cms.InputTag('goodPatJetsCA8PrunedPF'),
                                 topTagSrc = cms.InputTag('goodPatJetsCATopTagPF'),
+                                trigSrc = cms.InputTag('patTriggerEvent'),
+                                trigs = cms.vstring(
+                                    mytrigs
+                                    ),
                                 topTagParams = caTopTagParams.clone(
                                     tagName = cms.string('CATop')
                                     ),
@@ -38,7 +109,10 @@ process.ttbsmAna = cms.EDFilter('TTBSMProducer',
 
 process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
+print 'Making the path'
+
 process.p = cms.Path(
+    #process.patTriggerDefaultSequence*
     process.hltSelection*
     process.ttbsmAna
     )
