3cd3e214d1654a48fb704a71459630e8fd4367cf
srappocc <>
2011-07-01 17:53:07 +0000

    Removing TTBSMProducer that was for MC only. Merged it with the main one instead, controlled via configuration switches. Also added a specific MC-only configuration file

--- TTBSMPatTuples/plugins/TTBSMProducer.cc
+++ TTBSMPatTuples/plugins/TTBSMProducer.cc
@@ -13,7 +13,7 @@
 //
 // Original Author:  "Salvatore Rappoccio"
 //         Created:  Mon Jan 17 21:44:07 CST 2011
-// $Id: TTBSMProducer.cc,v 1.6 2011/06/28 17:44:46 srappocc Exp $
+// $Id: TTBSMProducer.cc,v 1.7 2011/06/28 18:52:30 srappocc Exp $
 //
 //
 
@@ -38,6 +38,24 @@
 #include "Analysis/BoostedTopAnalysis/interface/CATopTagFunctor.h"
 #include "Analysis/BoostedTopAnalysis/interface/BoostedTopWTagFunctor.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
+#include "SimDataFormats/PileupSummaryInfo/interface/PileupSummaryInfo.h" 
+#include "CondFormats/JetMETObjects/interface/JetCorrectionUncertainty.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+
+
+namespace LHAPDF {
+      void initPDFSet(int nset, const std::string& filename, int member=0);
+      int numberPDF(int nset);
+      void usePDFMember(int nset, int member);
+      double xfx(int nset, double x, double Q, int fl);
+      double getXmin(int nset, int member);
+      double getXmax(int nset, int member);
+      double getQ2min(int nset, int member);
+      double getQ2max(int nset, int member);
+      void extrapolate(bool extrapolate=true);
+}
+
 //
 // class declaration
 //
@@ -56,11 +74,26 @@ class TTBSMProducer : public edm::EDFilter {
   edm::InputTag             wTagSrc_; 
   edm::InputTag             topTagSrc_;
   edm::InputTag             trigSrc_;
+  edm::InputTag   rhoSrc_;          /// mean pt per unit area
+  edm::InputTag   pvSrc_;           /// primary vertex
   std::vector<std::string>  trigs_;
   std::string               topTagName_;
   CATopTagFunctor           topTagFunctor_;
   BoostedTopWTagFunctor     wTagFunctor_;
   bool                      readTrig_;
+  double                    scale_;
+  double                    smear_;
+  double                    etaSmear_;
+
+
+  std::vector<std::string>  jecPayloads_; /// files for JEC payloads
+  std::string               pdfSet_; /// lhapdf string
+
+  boost::shared_ptr<JetCorrectionUncertainty> jecUnc_;
+  boost::shared_ptr<FactorizedJetCorrector> jec_;
+
+
+
 };
 
 //
@@ -79,12 +112,37 @@ TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   wTagSrc_      (iConfig.getParameter<edm::InputTag>("wTagSrc") ),
   topTagSrc_    (iConfig.getParameter<edm::InputTag>("topTagSrc") ),
   trigSrc_      (iConfig.getParameter<edm::InputTag>("trigSrc") ),
+  rhoSrc_       ( iConfig.getParameter<edm::InputTag>("rhoSrc")),
+  pvSrc_        ( iConfig.getParameter<edm::InputTag>("pvSrc")),
   trigs_        (iConfig.getParameter<std::vector<std::string> > ("trigs") ),
   topTagName_   (iConfig.getParameter<edm::ParameterSet>("topTagParams").getParameter<std::string>("tagName") ),
   topTagFunctor_(iConfig.getParameter<edm::ParameterSet>("topTagParams")),
   wTagFunctor_  (iConfig.getParameter<edm::ParameterSet>("wTagParams")),
-  readTrig_     (iConfig.getParameter<bool>("readTrig"))
+  readTrig_     (iConfig.getParameter<bool>("readTrig")),
+  scale_        (iConfig.getParameter<double>( "jetScale" ) ),
+  smear_        (iConfig.getParameter<double>( "jetPtSmear") ),
+  etaSmear_     (iConfig.getParameter<double>( "jetEtaSmear") ),
+  jecPayloads_  (iConfig.getParameter<std::vector<std::string> >  ("jecPayloads")),
+  pdfSet_       (iConfig.getParameter<std::string> ("pdfSet") )
 {
+
+  // Get the factorized jet corrector. 
+  // The payloads contain N elements, 
+  // the Nth is the uncertainty, and the first N-1 elements are the
+  // actual correction levels. 
+  std::vector<JetCorrectorParameters> vPar;
+  for ( std::vector<std::string>::const_iterator ipayload = jecPayloads_.begin(),
+	  ipayloadEnd = jecPayloads_.end(); ipayload != ipayloadEnd - 1; ++ipayload ) {
+    std::cout << "Adding payload " << *ipayload << std::endl;
+    JetCorrectorParameters pars(*ipayload);
+    vPar.push_back(pars);
+  }
+
+  jec_ = boost::shared_ptr<FactorizedJetCorrector> ( new FactorizedJetCorrector(vPar) );
+
+  jecUnc_ = boost::shared_ptr<JetCorrectionUncertainty>( new JetCorrectionUncertainty(jecPayloads_.back()));
+
+
   //register your products
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4");
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("topTagP4");  
@@ -102,6 +160,8 @@ TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   produces<std::vector<std::string> > ("trigNames");
   produces<double> ("rho");
   produces<double> ("weight");
+  produces<unsigned int>    ("npv");
+  produces<int>    ("npvTrue");
 
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4Hemis0");
   produces<std::vector<reco::Candidate::PolarLorentzVector> > ("wTagP4Hemis1");
@@ -121,8 +181,11 @@ TTBSMProducer::TTBSMProducer(const edm::ParameterSet& iConfig) :
   produces<std::vector<int> >    ("topTagPassHemis1");
   produces<int>                  ("jet3Hemis0");
   produces<int>                  ("jet3Hemis1");
+  produces<std::vector<double> > ("pdfWeights");
 
 
+  if ( pdfSet_ != "" )
+    LHAPDF::initPDFSet(1, pdfSet_.c_str());
 }
 
 
@@ -139,6 +202,11 @@ TTBSMProducer::~TTBSMProducer()
 bool
 TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
+
+
+  
+  edm::Handle< std::vector<reco::Vertex> > h_pv;
+  iEvent.getByLabel( pvSrc_, h_pv );
 
   typedef std::vector<reco::Candidate::PolarLorentzVector> p4_vector;
   typedef reco::Candidate::PolarLorentzVector LorentzV;
@@ -179,13 +247,41 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   std::auto_ptr<std::vector<double> > topTagNSubjetsHemis1( new std::vector<double>() );
   std::auto_ptr<std::vector<int> > topTagPassHemis0  ( new std::vector<int>() );
   std::auto_ptr<std::vector<int> > topTagPassHemis1  ( new std::vector<int>() );
+  
+  std::auto_ptr<unsigned int> npv( new unsigned int() );
+  std::auto_ptr<int> npvTrue( new int() );
+  std::auto_ptr<std::vector<double> > pdf_weights( new std::vector<double>() );
 
+  // Number of reconstructed PV's
+  *npv = h_pv->size();
+
+  // Pileup reweighting in the MC
+  *npvTrue = -1;
+  if ( ! iEvent.isRealData() ) {
+    edm::InputTag PileupSrc_ ("addPileupInfo");
+    edm::Handle<std::vector< PileupSummaryInfo > >  PupInfo;
+    iEvent.getByLabel(PileupSrc_, PupInfo);
+    std::vector<PileupSummaryInfo>::const_iterator PVI;
+    // (then, for example, you can do)
+    for(PVI = PupInfo->begin(); PVI != PupInfo->end(); ++PVI) {
+      int BX = PVI->getBunchCrossing();
+      if(BX == 0) { 
+	*npvTrue = PVI->getPU_NumInteractions();
+	break;
+      }
+    }
+  }
 
 
 
   edm::Handle<std::vector<pat::Jet> > h_wTag;
   edm::Handle<std::vector<pat::Jet> > h_topTag;
   edm::Handle<pat::TriggerEvent>      h_trig;
+  edm::Handle<double> h_rho;
+
+  iEvent.getByLabel( rhoSrc_, h_rho );
+  double rhoVal = *h_rho;
+
 
   iEvent.getByLabel( wTagSrc_, h_wTag );
   iEvent.getByLabel( topTagSrc_, h_topTag );
@@ -194,10 +290,91 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   pat::strbitset topTagRet = topTagFunctor_.getBitTemplate();
   for ( std::vector<pat::Jet>::const_iterator jetBegin = h_wTag->begin(),
 	  jetEnd = h_wTag->end(), ijet = jetBegin; ijet != jetEnd; ++ijet ) {
-    wTagP4->push_back( ijet->polarP4() );
+
+
+    // -------
+    // Jet energy corrections
+    // -------
+    reco::Candidate::LorentzVector uncorrJet = ijet->correctedP4(0);
+    jec_->setJetEta( uncorrJet.eta() );
+    jec_->setJetPt ( uncorrJet.pt() );
+    jec_->setJetE  ( uncorrJet.energy() );
+    jec_->setJetA  ( ijet->jetArea() );
+    jec_->setRho   ( rhoVal );
+    jec_->setNPV   ( h_pv->size() );
+    double corr = jec_->getCorrection();
+
+
+    // -------
+    // Jet energy scale variation
+    // -------
+    if ( fabs(scale_) > 0.0001 ) {
+      // Now get the uncertainties
+      jecUnc_->setJetEta( uncorrJet.eta() );
+      jecUnc_->setJetPt( uncorrJet.pt() * corr );
+      double unc = fabs(jecUnc_->getUncertainty( bool(scale_ > 0) ));
+
+      // Add the "flat" flavor dependent corrections in quadrature
+      unc = sqrt( unc*unc + scale_*scale_);
+      double sign = 1.0;
+      if ( scale_ < 0.0 ) sign = -1.0;
+	     
+      // Scale up or down by jetScale_
+      double ijetscale = (1 + sign * unc);
+      corr *= ijetscale;
+    }    
+
+    // -------
+    // Jet angular resolution smearing
+    // -------
+    double etaScale_ = 1.0;
+    if( fabs(etaSmear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 ) {
+      double recoeta = ijet->eta();
+      double geneta = ijet->genJet()->eta();
+      double deltaeta = (recoeta-geneta)*etaSmear_ ;
+      etaScale_ = std::max((double)0.0,(recoeta+deltaeta)/recoeta);
+    }
+
+
+    double phiScale_ = 1.0;
+    if( fabs(etaSmear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 ) {
+      double recophi = ijet->phi();
+      double genphi = ijet->genJet()->phi();
+      double deltaphi = (recophi-genphi)*etaSmear_ ;
+      phiScale_ = std::max((double)0.0,(recophi+deltaphi)/recophi);
+    }
+
+
+    // -------
+    // Jet energy resolution smearing
+    // -------
+    double ptSmear_ = 1.0;
+    if( fabs(smear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 )  {
+      double recopt = ijet->pt();
+      double genpt = ijet->genJet()->pt();
+      double deltapt = (recopt-genpt)*smear_;
+      ptSmear_ = std::max((double)0.0, (recopt+deltapt)/recopt);
+    }
+
+    reco::Candidate::PolarLorentzVector corrJet (uncorrJet.pt(), uncorrJet.eta(), uncorrJet.phi(), uncorrJet.mass());
+    corrJet *=  (corr * ptSmear_);
+    corrJet.SetEta( corrJet.eta() * etaScale_ );
+    corrJet.SetPhi( corrJet.phi() * phiScale_ );
+
+    wTagP4->push_back( corrJet );
+    reco::Candidate::PolarLorentzVector corrDa0 = ijet->daughter(0)->polarP4() * (corr * ptSmear_);
+    reco::Candidate::PolarLorentzVector corrDa1 = ijet->daughter(1)->polarP4() * (corr * ptSmear_);
+
+    corrDa0.SetEta( corrDa0.eta() * etaScale_ );
+    corrDa1.SetEta( corrDa1.eta() * etaScale_ );
+
+    corrDa0.SetPhi( corrDa0.phi() * phiScale_ );
+    corrDa1.SetPhi( corrDa1.phi() * phiScale_ );
+
+
     bool passedWTag = wTagFunctor_(*ijet, wTagRet);
     double y = -1.0, mu = -1.0, dR = -1.0;
-    pat::subjetHelper( *ijet, y, mu, dR );
+    pat::subjetHelper( corrDa0, corrDa1, y, mu, dR, corrJet.mass() );
     wTagMu->push_back( mu );
     wTagPass->push_back( passedWTag );
     wTagBDisc->push_back( ijet->bDiscriminator("trackCountingHighEffBJetTags") );
@@ -205,13 +382,85 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   for ( std::vector<pat::Jet>::const_iterator jetBegin = h_topTag->begin(),
 	  jetEnd = h_topTag->end(), ijet = jetBegin; ijet != jetEnd; ++ijet ) {
+
+    // -------
+    // Jet energy corrections
+    // -------
+    reco::Candidate::LorentzVector uncorrJet = ijet->correctedP4(0);
+    jec_->setJetEta( uncorrJet.eta() );
+    jec_->setJetPt ( uncorrJet.pt() );
+    jec_->setJetE  ( uncorrJet.energy() );
+    jec_->setJetA  ( ijet->jetArea() );
+    jec_->setRho   ( rhoVal );
+    jec_->setNPV   ( h_pv->size() );
+    double corr = jec_->getCorrection();
+
+
+    // -------
+    // Jet energy scale variation
+    // -------
+    if ( fabs(scale_) > 0.0001 ) {
+      // Now get the uncertainties
+      jecUnc_->setJetEta( uncorrJet.eta() );
+      jecUnc_->setJetPt( uncorrJet.pt() * corr );
+      double unc = fabs(jecUnc_->getUncertainty( bool(scale_ > 0) ));
+
+      // Add the "flat" flavor dependent corrections in quadrature
+      unc = sqrt( unc*unc + scale_*scale_);
+      double sign = 1.0;
+      if ( scale_ < 0.0 ) sign = -1.0;
+	     
+      // Scale up or down by jetScale_
+      double ijetscale = (1 + sign * unc);
+      corr *= ijetscale;
+    }    
+
+    // -------
+    // Jet angular resolution smearing
+    // -------
+    double etaScale_ = 1.0;
+    if( fabs(etaSmear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 ) {
+      double recoeta = ijet->eta();
+      double geneta = ijet->genJet()->eta();
+      double deltaeta = (recoeta-geneta)*etaSmear_ ;
+      etaScale_ = std::max((double)0.0,(recoeta+deltaeta)/recoeta);
+    }
+
+
+    double phiScale_ = 1.0;
+    if( fabs(etaSmear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 ) {
+      double recophi = ijet->phi();
+      double genphi = ijet->genJet()->phi();
+      double deltaphi = (recophi-genphi)*etaSmear_ ;
+      phiScale_ = std::max((double)0.0,(recophi+deltaphi)/recophi);
+    }
+
+
+
+    // -------
+    // Jet energy resolution smearing
+    // -------
+    double ptSmear_ = 1.0;
+    if( fabs(smear_) > 0.0 && ijet->genJet() != 0 && ijet->genJet()->pt() > 15.0 )  {
+      double recopt = ijet->pt();
+      double genpt = ijet->genJet()->pt();
+      double deltapt = (recopt-genpt)*smear_;
+      ptSmear_ = std::max((double)0.0, (recopt+deltapt)/recopt);
+    }
+
+    reco::Candidate::PolarLorentzVector corrJet (uncorrJet.pt(), uncorrJet.eta(), uncorrJet.phi(), uncorrJet.mass());
+    corrJet *=  (corr * ptSmear_);
+    corrJet.SetEta( corrJet.eta() * etaScale_ );
+    corrJet.SetPhi( corrJet.phi() * etaScale_ );
+
     const reco::CATopJetTagInfo * catopTag = 
       dynamic_cast<reco::CATopJetTagInfo const *>(ijet->tagInfo(topTagName_));
     bool passedTopTag = topTagFunctor_( *ijet, topTagRet );
-    topTagP4->push_back( ijet->polarP4() );
+
+    topTagP4->push_back( corrJet );
     topTagPass->push_back( passedTopTag );
-    topTagMinMass->push_back( catopTag->properties().minMass );
-    topTagTopMass->push_back( catopTag->properties().topMass );
+    topTagMinMass->push_back( catopTag->properties().minMass * (corr * ptSmear_) );
+    topTagTopMass->push_back( catopTag->properties().topMass * (corr * ptSmear_) );
     topTagNSubjets->push_back( ijet->numberOfDaughters() );
     topTagBDisc->push_back( ijet->bDiscriminator("trackCountingHighEffBJetTags") );
 
@@ -298,15 +547,46 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
     }
   }
 
-  edm::Handle<double>        caTopJetRho;
-  iEvent.getByLabel( edm::InputTag("kt6PFJetsPFlow", "rho"),    caTopJetRho );
-  *rho = *caTopJetRho ;
+
+  *rho = rhoVal ;
+
+
   edm::Handle<GenEventInfoProduct>    genEvt;
   iEvent.getByLabel( edm::InputTag("generator"),  genEvt );
   if( genEvt.isValid() )  {
     *weight = genEvt->weight() ;
   }
 
+  if ( ! iEvent.isRealData() && pdfSet_ != "" ) {
+
+    edm::Handle<GenEventInfoProduct> pdfstuff;
+    if (iEvent.getByLabel("generator", pdfstuff)) {
+
+
+      LHAPDF::usePDFMember(1,0);
+
+      float q = pdfstuff->pdf()->scalePDF;
+ 
+      int id1 = pdfstuff->pdf()->id.first;
+      double x1 = pdfstuff->pdf()->x.first;
+      // double pdf1 = pdfstuff->pdf()->xPDF.first;
+      
+      int id2 = pdfstuff->pdf()->id.second;
+      double x2 = pdfstuff->pdf()->x.second;
+      // double pdf2 = pdfstuff->pdf()->xPDF.second; 
+
+      double xpdf1 = LHAPDF::xfx(1, x1, q, id1);
+      double xpdf2 = LHAPDF::xfx(1, x2, q, id2);
+      double w0 = xpdf1 * xpdf2;
+      for(int i=1; i <=44; ++i){
+	LHAPDF::usePDFMember(1,i);
+	double xpdf1_new = LHAPDF::xfx(1, x1, q, id1);
+	double xpdf2_new = LHAPDF::xfx(1, x2, q, id2);
+	double weight = xpdf1_new * xpdf2_new / w0;
+	pdf_weights->push_back(weight);
+      }
+    }
+  }
 
   iEvent.put(wTagP4        ,"wTagP4");
   iEvent.put(topTagP4      ,"topTagP4");  
@@ -342,6 +622,9 @@ TTBSMProducer::filter(edm::Event& iEvent, const edm::EventSetup& iSetup)
   iEvent.put(jet3Hemis1,          "jet3Hemis1"  );
   iEvent.put( rho,                "rho" );
   iEvent.put( weight,             "weight");
+  iEvent.put( npv,     "npv");
+  iEvent.put( npvTrue, "npvTrue");
+  iEvent.put( pdf_weights, "pdfWeights");
 
   return true;
 }
