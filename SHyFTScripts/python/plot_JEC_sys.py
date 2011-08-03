827622ebc6ca3bf76206969f24919ce963a95dfe
skhalil <>
2011-08-03 03:16:58 +0000

    updating the systematic plots macros

--- SHyFTScripts/python/plot_JEC_sys.py
+++ SHyFTScripts/python/plot_JEC_sys.py
@@ -1,51 +1,63 @@
 #!/bin/python
 
-
-
 from ROOT import *
 from array import *
-
-gROOT.Macro("~/rootlogon.C")
-
+from setTDRStyle import *
+setTDRStyle()
+#gROOT.Macro("~/rootlogon.C")
+#gStyle.SetCanvasColor(10)
+#gStyle.SetStatColor(10)
+gStyle.SetTitleFillColor(10)
+gStyle.SetTitleBorderSize(0)
 gStyle.SetOptStat(000000)
-f = TFile('TTJets_TuneD6T_7TeV-madgraph-tauola_shyftana_387_v1.root')
 
-leg = TLegend(0.6, 0.6, 0.84, 0.84)
+f = TFile('../RootFiles_v5/TTJets_TuneZ2_7TeV-madgraph-tauola_tlbsm_424_v8.root')
+
+leg = TLegend(0.65, 0.65, 0.88, 0.88)
 leg.SetBorderSize(0)
 leg.SetFillColor(0)
 
 c1 = TCanvas('c1', 'c1')
-h_njets_1 = f.Get('pfRecoShyftAnaJES095/nJets')
-h_njets_2 = f.Get('pfRecoShyftAna/nJets')
-h_njets_3 = f.Get('pfRecoShyftAnaJES105/nJets')
+h_njets_1 = f.Get('pfShyftAnaJES095/nJets')
+h_njets_2 = f.Get('pfShyftAna/nJets')
+h_njets_3 = f.Get('pfShyftAnaJES105/nJets')
 
-h_njets_3.SetTitle("NJets, p_{T} > 25 GeV, |#eta| < 2.4;N_{JETS}")
+h_njets_3.SetTitle("")
+h_njets_3.GetXaxis().SetTitle("N_{Jets}; p_{T} > 30 GeV, |#eta| < 2.4")
 
 h_njets_3.SetLineColor(4)
 h_njets_2.SetLineColor(1)
 h_njets_1.SetLineColor(2)
 
-leg.AddEntry(h_njets_3, 'JEC Down', 'l')
+leg.AddEntry(h_njets_1, 'JEC Down', 'l')
 leg.AddEntry(h_njets_2, 'JEC Nominal', 'l')
-leg.AddEntry(h_njets_1, 'JEC Up', 'l')
+leg.AddEntry(h_njets_3, 'JEC Up', 'l')
 
 h_njets_3.Draw('hist')
 h_njets_2.Draw('hist same')
 h_njets_1.Draw('hist same')
 leg.Draw()
-c1.Print('jec_sys_njets_ele.png', 'png')
+
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#latex.DrawLatex(0.6,0.92,"#int Ldt = 771 pb^{-1}")
+
+#c1.Print('jec_sys_njets_ele.png', 'png')
 c1.Print('jec_sys_njets_ele.pdf', 'pdf')
 
 c2 = TCanvas('c2', 'c2')
-h_jet1Pt_1 = f.Get('pfRecoShyftAnaJES095/jet1Pt')
-h_jet1Pt_2 = f.Get('pfRecoShyftAna/jet1Pt')
-h_jet1Pt_3 = f.Get('pfRecoShyftAnaJES105/jet1Pt')
+h_jet1Pt_1 = f.Get('pfShyftAnaJES095/jet1Pt')
+h_jet1Pt_2 = f.Get('pfShyftAna/jet1Pt')
+h_jet1Pt_3 = f.Get('pfShyftAnaJES105/jet1Pt')
 
 h_jet1Pt_1.Rebin(5)
 h_jet1Pt_2.Rebin(5)
 h_jet1Pt_3.Rebin(5)
 
-h_jet1Pt_3.SetTitle("Jet 1 p_{T};p_{T} (GeV/c)")
+h_jet1Pt_3.SetTitle("")
+h_jet1Pt_3.GetXaxis().SetTitle("Jet 1; p_{T} (GeV/c)")
 
 h_jet1Pt_3.SetLineColor(4)
 h_jet1Pt_2.SetLineColor(1)
@@ -55,18 +67,26 @@ h_jet1Pt_3.Draw('hist')
 h_jet1Pt_2.Draw('hist same')
 h_jet1Pt_1.Draw('hist same')
 leg.Draw()
-c2.Print('jec_sys_jet1Pt_ele.png', 'png')
+
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#c2.Print('jec_sys_jet1Pt_ele.png', 'png')
 c2.Print('jec_sys_jet1Pt_ele.pdf', 'pdf')
 
 
 c3 = TCanvas('c3', 'c3')
-h_jet2Pt_1 = f.Get('pfRecoShyftAnaJES095/jet2Pt')
-h_jet2Pt_2 = f.Get('pfRecoShyftAna/jet2Pt')
-h_jet2Pt_3 = f.Get('pfRecoShyftAnaJES105/jet2Pt')
+h_jet2Pt_1 = f.Get('pfShyftAnaJES095/jet2Pt')
+h_jet2Pt_2 = f.Get('pfShyftAna/jet2Pt')
+h_jet2Pt_3 = f.Get('pfShyftAnaJES105/jet2Pt')
 
 h_jet2Pt_1.Rebin(5)
 h_jet2Pt_2.Rebin(5)
 h_jet2Pt_3.Rebin(5)
+
+h_jet2Pt_3.SetTitle("")
+h_jet2Pt_3.GetXaxis().SetTitle("Jet 2; p_{T} (GeV/c)")
 
 h_jet2Pt_3.SetLineColor(4)
 h_jet2Pt_2.SetLineColor(1)
@@ -76,17 +96,24 @@ h_jet2Pt_3.Draw('hist')
 h_jet2Pt_2.Draw('hist same')
 h_jet2Pt_1.Draw('hist same')
 leg.Draw()
-c3.Print('jec_sys_jet2Pt_ele.png', 'png')
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#c3.Print('jec_sys_jet2Pt_ele.png', 'png')
 c3.Print('jec_sys_jet2Pt_ele.pdf', 'pdf')
 
-lsc4 = TCanvas('c4', 'c4')
-h_jet3Pt_1 = f.Get('pfRecoShyftAnaJES095/jet3Pt')
-h_jet3Pt_2 = f.Get('pfRecoShyftAna/jet3Pt')
-h_jet3Pt_3 = f.Get('pfRecoShyftAnaJES105/jet3Pt')
+c4 = TCanvas('c4', 'c4')
+h_jet3Pt_1 = f.Get('pfShyftAnaJES095/jet3Pt')
+h_jet3Pt_2 = f.Get('pfShyftAna/jet3Pt')
+h_jet3Pt_3 = f.Get('pfShyftAnaJES105/jet3Pt')
 
 h_jet3Pt_1.Rebin(5)
 h_jet3Pt_2.Rebin(5)
 h_jet3Pt_3.Rebin(5)
+
+h_jet3Pt_3.SetTitle("")
+h_jet3Pt_3.GetXaxis().SetTitle("Jet 3; p_{T} (GeV/c)")
 
 h_jet3Pt_3.SetLineColor(4)
 h_jet3Pt_2.SetLineColor(1)
@@ -96,14 +123,47 @@ h_jet3Pt_3.Draw('hist')
 h_jet3Pt_2.Draw('hist same')
 h_jet3Pt_1.Draw('hist same')
 leg.Draw()
-c4.Print('jec_sys_jet3Pt_ele.png', 'png')
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary" )
+#c4.Print('jec_sys_jet3Pt_ele.png', 'png')
 c4.Print('jec_sys_jet3Pt_ele.pdf', 'pdf')
 
 
-c5 = TCanvas('c5', 'c5')
-h_metPt_1 = f.Get('pfRecoShyftAnaJES095/metPt')
-h_metPt_2 = f.Get('pfRecoShyftAna/metPt')
-h_metPt_3 = f.Get('pfRecoShyftAnaJES105/metPt')
+c5 = TCanvas('c4', 'c4')
+h_jet4Pt_1 = f.Get('pfShyftAnaJES095/jet4Pt')
+h_jet4Pt_2 = f.Get('pfShyftAna/jet4Pt')
+h_jet4Pt_3 = f.Get('pfShyftAnaJES105/jet4Pt')
+
+h_jet4Pt_1.Rebin(5)
+h_jet4Pt_2.Rebin(5)
+h_jet4Pt_3.Rebin(5)
+
+h_jet4Pt_3.SetTitle("")
+h_jet4Pt_3.GetXaxis().SetTitle("Jet 4; p_{T} (GeV/c)")
+
+h_jet4Pt_3.SetLineColor(4)
+h_jet4Pt_2.SetLineColor(1)
+h_jet4Pt_1.SetLineColor(2)
+
+h_jet4Pt_3.Draw('hist')
+h_jet4Pt_2.Draw('hist same')
+h_jet4Pt_1.Draw('hist same')
+leg.Draw()
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#latex.DrawLatex(0.6,0.92,"#int Ldt = 771 pb^{-1}")
+#c5.Print('jec_sys_jet4Pt_ele.png', 'png')
+c5.Print('jec_sys_jet4Pt_ele.pdf', 'pdf')
+
+
+c6 = TCanvas('c6', 'c6')
+h_metPt_1 = f.Get('pfShyftAnaJES095/metPt')
+h_metPt_2 = f.Get('pfShyftAna/metPt')
+h_metPt_3 = f.Get('pfShyftAnaJES105/metPt')
 
 h_metPt_1.Rebin(5)
 h_metPt_2.Rebin(5)
@@ -113,9 +173,16 @@ h_metPt_3.SetLineColor(4)
 h_metPt_2.SetLineColor(1)
 h_metPt_1.SetLineColor(2)
 
+h_metPt_3.SetTitle("")
+h_metPt_3.GetXaxis().SetTitle("#slash{E}_{T}(GeV/c)")
+
 h_metPt_3.Draw('hist')
 h_metPt_2.Draw('hist same')
 h_metPt_1.Draw('hist same')
 leg.Draw()
-c5.Print('jec_sys_metPt_ele.png', 'png')
-c5.Print('jec_sys_metPt_ele.pdf', 'pdf')
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#c6.Print('jec_sys_metPt_ele.png', 'png')
+c6.Print('jec_sys_metPt_ele.pdf', 'pdf')
--- SHyFTScripts/python/plot_MET_sys.py
+++ SHyFTScripts/python/plot_MET_sys.py
@@ -1,25 +1,29 @@
 #!/bin/python
 
-
-
 from ROOT import *
 from array import *
-
-gROOT.Macro("~/rootlogon.C")
-
+from setTDRStyle import *
+setTDRStyle()
+#gROOT.Macro("~/rootlogon.C")
+#gStyle.SetCanvasColor(10)
+#gStyle.SetStatColor(10)
+gStyle.SetTitleFillColor(10)
+gStyle.SetTitleBorderSize(0)
 gStyle.SetOptStat(000000)
-f = TFile('TTJets_TuneD6T_7TeV-madgraph-tauola_shyftana_387_v1.root')
 
-leg = TLegend(0.6, 0.6, 0.84, 0.84)
+f = TFile('../RootFiles_v5/TTJets_TuneZ2_7TeV-madgraph-tauola_tlbsm_424_v8.root')
+
+leg = TLegend(0.65, 0.65, 0.88, 0.88)
 leg.SetBorderSize(0)
 leg.SetFillColor(0)
 
 c1 = TCanvas('c1', 'c1')
-h_njets_1 = f.Get('pfRecoShyftAnaMETRES090/nJets')
-h_njets_2 = f.Get('pfRecoShyftAna/nJets')
-h_njets_3 = f.Get('pfRecoShyftAnaMETRES110/nJets')
+h_njets_1 = f.Get('pfShyftAnaMETRES090/nJets')
+h_njets_2 = f.Get('pfShyftAna/nJets')
+h_njets_3 = f.Get('pfShyftAnaMETRES110/nJets')
 
-h_njets_3.SetTitle("NJets, p_{T} > 25 GeV, |#eta| < 2.4;N_{JETS}")
+h_njets_3.SetTitle("")
+h_njets_3.GetXaxis().SetTitle("N_{Jets}; p_{T} > 30 GeV, |#eta| < 2.4")
 
 h_njets_3.SetLineColor(4)
 h_njets_2.SetLineColor(1)
@@ -33,14 +37,21 @@ h_njets_3.Draw('hist')
 h_njets_2.Draw('hist same')
 h_njets_1.Draw('hist same')
 leg.Draw()
-c1.Print('unclmet_sys_njets_ele.png', 'png')
+
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#latex.DrawLatex(0.6,0.92,"#int Ldt = 771 pb^{-1}")
+
+#c1.Print('unclmet_sys_njets_ele.png', 'png')
 c1.Print('unclmet_sys_njets_ele.pdf', 'pdf')
 
 
 c5 = TCanvas('c5', 'c5')
-h_metPt_1 = f.Get('pfRecoShyftAnaMETRES090/metPt')
-h_metPt_2 = f.Get('pfRecoShyftAna/metPt')
-h_metPt_3 = f.Get('pfRecoShyftAnaMETRES110/metPt')
+h_metPt_1 = f.Get('pfShyftAnaMETRES090/metPt')
+h_metPt_2 = f.Get('pfShyftAna/metPt')
+h_metPt_3 = f.Get('pfShyftAnaMETRES110/metPt')
 
 h_metPt_1.Rebin(5)
 h_metPt_2.Rebin(5)
@@ -50,9 +61,18 @@ h_metPt_3.SetLineColor(4)
 h_metPt_2.SetLineColor(1)
 h_metPt_1.SetLineColor(2)
 
+h_metPt_3.SetTitle("")
+h_metPt_3.GetXaxis().SetTitle("#slash{E}_{T}(GeV/c)")
+
 h_metPt_3.Draw('hist')
 h_metPt_2.Draw('hist same')
 h_metPt_1.Draw('hist same')
 leg.Draw()
-c5.Print('unclmet_sys_metPt_ele.png', 'png')
+
+latex = TLatex()
+latex.SetNDC()
+latex.SetTextSize(0.04)
+latex.DrawLatex(0.6,0.96, "CMS Preliminary")
+#latex.DrawLatex(0.6,0.92,"#int Ldt = 771 pb^{-1}")
+#c5.Print('unclmet_sys_metPt_ele.png', 'png')
 c5.Print('unclmet_sys_metPt_ele.pdf', 'pdf')
