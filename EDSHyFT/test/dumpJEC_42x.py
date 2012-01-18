b472cf83c0ccab2c7aea7fc415001740db4bcd8c
skhalil <>
2012-01-18 20:08:56 +0000

    adding new JEC payloads

--- EDSHyFT/test/dumpJEC_42x.py
+++ EDSHyFT/test/dumpJEC_42x.py
@@ -18,13 +18,13 @@ from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
 
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string( 'GR_R_42_V19::All' )
+process.GlobalTag.globaltag = cms.string( 'GR_R_42_V23::All' )
 ##-------------------- Import the JEC services -----------------------
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
 process.readAK5PFchs = cms.EDAnalyzer('JetCorrectorDBReader', 
                                    payloadName    = cms.untracked.string('AK5PFchs'),
                                    printScreen    = cms.untracked.bool(False),
                                    createTextFile = cms.untracked.bool(True),
-                                   globalTag      = cms.untracked.string('Jec11_V2')
+                                   globalTag      = cms.untracked.string('Jec12_V1')
                                    )
 process.p = cms.Path( process.readAK5PFchs )
