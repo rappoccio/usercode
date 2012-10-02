49a2c167aac7d9ee7ff6b9bcd75f4be09449d8da
skhalil <>
2012-10-02 21:16:59 +0000

    script to create ntuples on condor

---
+++ EDSHyFT/test/condorJobs_8TeV/condor.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo "input parameters: cluster, process, run path, input path, output path, data?, leptype " $1 $2 $3 $4 $5 $6
+
+CLUSTER=$1
+PROCESS=$2
+RUNPATH=$3
+INPATH=$4
+OUTPATH=$5 
+DATA=$6
+
+
+echo ""
+echo "CMSSW on Condor"
+echo ""
+
+START_TIME=`/bin/date`
+echo "started at $START_TIME"
+
+echo ""
+echo "parameter set:"
+echo "CLUSTER: $CLUSTER"
+echo "PROCESS: $PROCESS"
+echo "RUNPATH: $RUNPATH"
+echo "INPATH: $INPATH"
+echo "OUTPATH: $OUTPATH"
+echo "DATA: $DATA"
+
+cd $RUNPATH
+source /uscmst1/prod/sw/cms/shrc uaf
+eval `scramv1 runtime -sh`
+
+counter=0
+
+
+for txt in `ls $INPATH`; do
+    echo "going here: "$txt
+    if test -f $INPATH/$txt; then
+        echo "file   $INPATH/$txt   exists"
+        name = 'basename $txt .txt'
+        echo "output file name: $name "
+        if test $DATA -eq 0; then
+            if test $counter -eq $PROCESS; then
+                echo "python ntupleMaker.py --onDcache --txtfiles $INPATH/$txt --sample $OUTPATH/${name}_nominal"
+                python ntupleMaker.py --onDcache --txtfiles $INPATH/$txt --sample $OUTPATH/${name}_nominal
+            fi
+            let "counter+=1"
+        else
+            let "counter+=1"
+        fi
+
+    fi
+done
