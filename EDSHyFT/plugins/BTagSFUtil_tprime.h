8d9b8ab7ae2a8bd1143acde76150a85cc7b8b06a
ferencek <>
2012-10-02 04:08:30 +0000

    cleaned up code and modified it to work with MC efficiencies instead of data efficiencies

--- EDSHyFT/plugins/BTagSFUtil_tprime.h
+++ EDSHyFT/plugins/BTagSFUtil_tprime.h
@@ -2,19 +2,17 @@
 
 Class Usage:
 
-This class should only be used for upgrading and downgrading 
-if a single operating point is used in an analysis. 
+This class should only be used for upgrading and downgrading
+if a single operating point is used in an analysis.
 
-bool isBTagged = b-tag flag for jetBTagSFUtil-tprime
-int pdgIdPart = parton idBTagSFUtil-tprime
-float Btag_SF = MC/data scale factor for b/c-tagging efficiency
-float Btag_eff = b/c-tagging efficiency in data
-float Bmistag_SF = MC/data scale factor for mistag efficiency
-float Bmistag_eff = mistag efficiency in data
+bool isBTagged = b-tag flag for jet
+float Btag_SF = data/MC scale factor for b-tagging efficiency
+float Btag_eff = b-tagging efficiency in MC
 
 Author: Michael Segala
 Contact: michael.segala@gmail.com
 Updated: Ulrich Heintz 12/23/2011
+Modified: Dinko Ferencek 10/01/2012
 
 *************************************************************/
 
@@ -30,17 +28,14 @@ Updated: Ulrich Heintz 12/23/2011
 class BTagSFUtil{
 
  public:
-    
+
   BTagSFUtil( int seed=0 );
   ~BTagSFUtil();
-    
-  void modifyBTagsWithSF( bool& isBTagged, int pdgIdPart, float Btag_SF = 0.98, float Btag_eff = 1.0, float Bmistag_SF = 1.0, float  Bmistag_eff = 1.0);
 
+  bool applySF(bool isBTagged, float Btag_SF = 0.98, float Btag_eff = 1.0);
 
  private:
-  
-  bool applySF(bool& isBTagged, float Btag_SF = 0.98, float Btag_eff = 1.0);
-  
+
   TRandom3* rand_;
 
 };
@@ -58,52 +53,28 @@ BTagSFUtil::~BTagSFUtil() {
 
 }
 
-
-void BTagSFUtil::modifyBTagsWithSF(bool& isBTagged, int pdgIdPart, float Btag_SF, float Btag_eff, float Bmistag_SF, float Bmistag_eff){
+bool BTagSFUtil::applySF(bool isBTagged, float Btag_SF, float Btag_eff){
 
   bool newBTag = isBTagged;
 
-  // b quarks and c quarks:
-  if( abs( pdgIdPart ) == 5 ||  abs( pdgIdPart ) == 4) { 
-
-    double bctag_eff = Btag_eff;
-    if ( abs(pdgIdPart)==4 )  bctag_eff = Btag_eff/5; // take ctag eff as one 5th of Btag eff
-    newBTag = applySF(isBTagged, Btag_SF, bctag_eff);
-
-  // light quarks:
-  } else if( abs( pdgIdPart )>0 ) { //in data it is 0 (save computing time)
-
-    newBTag = applySF(isBTagged, Bmistag_SF, Bmistag_eff);
-    
-  }
-
-  isBTagged = newBTag;
-
-}
-
-
-bool BTagSFUtil::applySF(bool& isBTagged, float Btag_SF, float Btag_eff){
-  
-  bool newBTag = isBTagged;
-
-  if (Btag_SF == 1) return newBTag; //no correction needed 
+  if (Btag_SF == 1) return newBTag; //no correction needed
 
   //throw die
-  float coin = rand_->Uniform(1.);    
-  
+  float coin = rand_->Uniform(1.);
+
   if(Btag_SF > 1){  // use this if SF>1
 
     if( !isBTagged ) {
 
       //fraction of jets that need to be upgraded
-      float mistagPercent = (1.0 - Btag_SF) / (1.0 - (Btag_SF/Btag_eff) );
+      float mistagPercent = (1.0 - Btag_SF) / (1.0 - (1.0/Btag_eff) );
 
       //upgrade to tagged
       if( coin < mistagPercent ) {newBTag = true;}
     }
 
   }else{  // use this if SF<1
-      
+
     //downgrade tagged to untagged
     if( isBTagged && coin > Btag_SF ) {newBTag = false;}
 
