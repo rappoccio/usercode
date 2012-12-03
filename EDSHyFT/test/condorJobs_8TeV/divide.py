4c03aa547551f09b2413a386a2e6018db0a0506c
skhalil <>
2012-12-03 23:26:13 +0000

    with better options

--- EDSHyFT/test/condorJobs_8TeV/divide.py
+++ EDSHyFT/test/condorJobs_8TeV/divide.py
@@ -6,26 +6,36 @@ import itertools
 import sys
 import math
 
-input = 'SingleEle-Run2012B-13Jul2012'
-max_jobs = int(sys.argv[1] if 1 < len(sys.argv) else 10)
-
 def get_num_input_files(filename):
     return sum(1 for line in open(filename, 'r'))
 
-num_input_files = get_num_input_files(input+'.txt')
-inputs_per_job = math.ceil(num_input_files / max_jobs)
+path='Unsplit/'
 
-print('inputs_per_jobs', inputs_per_job)
+textFiles = [
+    [path+'SingleEle-Run2012A-13Jul2012', 4],
+    [path+'SingleEle-Run2012B-13Jul2012', 16],
+    [path+'SingleEle-Run2012A-06Aug2012', 4],
+    [path+'SingleEle-Run2012C-24Aug2012', 4],
+    ]
+
+for file in textFiles:
+    input = file[0]
+    max_jobs = file[1]
+    
+    num_input_files = get_num_input_files(input+'.txt')
+    inputs_per_job = math.ceil(num_input_files / max_jobs)
+    
+    print('inputs_per_jobs', inputs_per_job)
 
 
-for job in range(0, max_jobs):
-    lines = []
-    for line in itertools.islice(open(input+'.txt', 'r'),
-                     job * inputs_per_job,
-                     (job + 1) * inputs_per_job):
-        lines.append(line.strip())
-
-    if lines:
-        with open(input+'_v'+str(job)+'.txt', 'w') as output_:
-            for line in lines:
-                print(line, file=output_)
+    for job in range(0, max_jobs):
+        lines = []
+        for line in itertools.islice(open(input+'.txt', 'r'),
+                                     job * inputs_per_job,
+                                     (job + 1) * inputs_per_job):
+            lines.append(line.strip())
+            
+        if lines:
+            with open(input+'_v'+str(job)+'.txt', 'w') as output_:
+                for line in lines:
+                    print(line, file=output_)
