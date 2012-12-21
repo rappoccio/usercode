4609c4294367e16ca48f6db670bd13f512c5d0e7
ferencek <>
2012-12-21 12:10:11 +0000

    updated to the latest BTV POG recommendations (for Moriond 2012)

--- EDSHyFT/plugins/BTaggingSFProducer.cc
+++ EDSHyFT/plugins/BTaggingSFProducer.cc
@@ -21,12 +21,12 @@ class BTaggingSFProducer : public edm::EDProducer {
 public:
   explicit BTaggingSFProducer(const edm::ParameterSet&);
   ~BTaggingSFProducer();
-  
+
 private:
   virtual void beginJob() ;
   virtual void produce(edm::Event&, const edm::EventSetup&);
   virtual void endJob() ;
-  
+
   // ----------member data --------------------------
   edm::InputTag jetSource_;
   std::string discriminatorTag_;
@@ -51,9 +51,6 @@ private:
   TF1 *CSVM_SFl_0p8to1p6_max;
   TF1 *CSVM_SFl_1p6to2p4_max;
 
-  double SFb_Unc_MultFactor;
-  TF1 *CSVM_SFl_Corr;
-
   TFile *f_EffMap;
 
   TH2D *h2_EffMapB;
@@ -68,45 +65,42 @@ BTaggingSFProducer::BTaggingSFProducer(const edm::ParameterSet& iConfig)
   discriminatorValue_ = iConfig.getParameter<double>("DiscriminatorValue");
   effMapFile_ = iConfig.getParameter<std::string>("EffMapFile");
 
-  double PtBins[] = {30, 40, 50, 60, 70, 80, 100, 120, 160, 210, 260, 320, 400, 500, 670};
+  double PtBins[] = {20, 30, 40, 50, 60, 70, 80, 100, 120, 160, 210, 260, 320, 400, 500, 600, 800};
 
-  CSVM_SFb_0to2p4 = new TF1("CSVM_SFb_0to2p4","0.6981*((1.+(0.414063*x))/(1.+(0.300155*x)))", 30.,670.);
+  CSVM_SFb_0to2p4 = new TF1("CSVM_SFb_0to2p4","0.726981*((1.+(0.253238*x))/(1.+(0.188389*x)))", 20.,800.);
 
-  CSVM_SFb_errors = new TH1D("CSVM_SFb_errors", "CSVM_SFb_errors", 14, PtBins);
-  CSVM_SFb_errors->SetBinContent( 0,0.12);
-  CSVM_SFb_errors->SetBinContent( 1,0.0295675);
-  CSVM_SFb_errors->SetBinContent( 2,0.0295095);
-  CSVM_SFb_errors->SetBinContent( 3,0.0210867);
-  CSVM_SFb_errors->SetBinContent( 4,0.0219349);
-  CSVM_SFb_errors->SetBinContent( 5,0.0227033);
-  CSVM_SFb_errors->SetBinContent( 6,0.0204062);
-  CSVM_SFb_errors->SetBinContent( 7,0.0185857);
-  CSVM_SFb_errors->SetBinContent( 8,0.0256242);
-  CSVM_SFb_errors->SetBinContent( 9,0.0383341);
-  CSVM_SFb_errors->SetBinContent(10,0.0409675);
-  CSVM_SFb_errors->SetBinContent(11,0.0420284);
-  CSVM_SFb_errors->SetBinContent(12,0.0541299);
-  CSVM_SFb_errors->SetBinContent(13,0.0578761);
-  CSVM_SFb_errors->SetBinContent(14,0.0655432);
-  CSVM_SFb_errors->SetBinContent(15,(2*0.0655432));
+  CSVM_SFb_errors = new TH1D("CSVM_SFb_errors", "CSVM_SFb_errors", 16, PtBins);
+  CSVM_SFb_errors->SetBinContent( 0,(2*0.0554504));
+  CSVM_SFb_errors->SetBinContent( 1,0.0554504);
+  CSVM_SFb_errors->SetBinContent( 2,0.0209663);
+  CSVM_SFb_errors->SetBinContent( 3,0.0207019);
+  CSVM_SFb_errors->SetBinContent( 4,0.0230073);
+  CSVM_SFb_errors->SetBinContent( 5,0.0208719);
+  CSVM_SFb_errors->SetBinContent( 6,0.0200453);
+  CSVM_SFb_errors->SetBinContent( 7,0.0264232);
+  CSVM_SFb_errors->SetBinContent( 8,0.0240102);
+  CSVM_SFb_errors->SetBinContent( 9,0.0229375);
+  CSVM_SFb_errors->SetBinContent(10,0.0184615);
+  CSVM_SFb_errors->SetBinContent(11,0.0216242);
+  CSVM_SFb_errors->SetBinContent(12,0.0248119);
+  CSVM_SFb_errors->SetBinContent(13,0.0465748);
+  CSVM_SFb_errors->SetBinContent(14,0.0474666);
+  CSVM_SFb_errors->SetBinContent(15,0.0718173);
+  CSVM_SFb_errors->SetBinContent(16,0.0717567);
+  CSVM_SFb_errors->SetBinContent(17,(2*0.0717567));
 
-  CSVM_SFl_0to2p4 =   new TF1("CSVM_SFl_0to2p4","((1.04318+(0.000848162*x))+(-2.5795e-06*(x*x)))+(1.64156e-09*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0to0p8 =   new TF1("CSVM_SFl_0to0p8","((1.06182+(0.000617034*x))+(-1.5732e-06*(x*x)))+(3.02909e-10*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0p8to1p6 = new TF1("CSVM_SFl_0p8to1p6","((1.111+(-9.64191e-06*x))+(1.80811e-07*(x*x)))+(-5.44868e-10*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_1p6to2p4 = new TF1("CSVM_SFl_1p6to2p4","((1.08498+(-0.000701422*x))+(3.43612e-06*(x*x)))+(-4.11794e-09*(x*(x*x)))", 20.,670.);
+  // light flavor SFs are provided separately for 2012 data taking periods AB, C, and D as well as full ABCD. Values used here correspond to the full period ABCD
+  CSVM_SFl_0to0p8 =   new TF1("CSVM_SFl_0to0p8","((1.06238+(0.00198635*x))+(-4.89082e-06*(x*x)))+(3.29312e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_0p8to1p6 = new TF1("CSVM_SFl_0p8to1p6","((1.08048+(0.00110831*x))+(-2.96189e-06*(x*x)))+(2.16266e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_1p6to2p4 = new TF1("CSVM_SFl_1p6to2p4","((1.09145+(0.000687171*x))+(-2.45054e-06*(x*x)))+(1.7844e-09*(x*(x*x)))", 20.,700.);
 
-  CSVM_SFl_0to2p4_min =   new TF1("CSVM_SFl_0to2p4_min","((0.962627+(0.000448344*x))+(-1.25579e-06*(x*x)))+(4.82283e-10*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0to0p8_min =   new TF1("CSVM_SFl_0to0p8_min","((0.972455+(7.51396e-06*x))+(4.91857e-07*(x*x)))+(-1.47661e-09*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0p8to1p6_min = new TF1("CSVM_SFl_0p8to1p6_min","((1.02055+(-0.000378856*x))+(1.49029e-06*(x*x)))+(-1.74966e-09*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_1p6to2p4_min = new TF1("CSVM_SFl_1p6to2p4_min","((0.983476+(-0.000607242*x))+(3.17997e-06*(x*x)))+(-4.01242e-09*(x*(x*x)))", 20.,670.);
+  CSVM_SFl_0to0p8_min =   new TF1("CSVM_SFl_0to0p8_min","((0.972746+(0.00104424*x))+(-2.36081e-06*(x*x)))+(1.53438e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_0p8to1p6_min = new TF1("CSVM_SFl_0p8to1p6_min","((0.9836+(0.000649761*x))+(-1.59773e-06*(x*x)))+(1.14324e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_1p6to2p4_min = new TF1("CSVM_SFl_1p6to2p4_min","((1.00616+(0.000358884*x))+(-1.23768e-06*(x*x)))+(6.86678e-10*(x*(x*x)))", 20.,700.);
 
-  CSVM_SFl_0to2p4_max =   new TF1("CSVM_SFl_0to2p4_max","((1.12368+(0.00124806*x))+(-3.9032e-06*(x*x)))+(2.80083e-09*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0to0p8_max =   new TF1("CSVM_SFl_0to0p8_max","((1.15116+(0.00122657*x))+(-3.63826e-06*(x*x)))+(2.08242e-09*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_0p8to1p6_max = new TF1("CSVM_SFl_0p8to1p6_max","((1.20146+(0.000359543*x))+(-1.12866e-06*(x*x)))+(6.59918e-10*(x*(x*x)))", 20.,670.);
-  CSVM_SFl_1p6to2p4_max = new TF1("CSVM_SFl_1p6to2p4_max","((1.18654+(-0.000795808*x))+(3.69226e-06*(x*x)))+(-4.22347e-09*(x*(x*x)))", 20.,670.);
-
-  SFb_Unc_MultFactor = 1.5;
-  CSVM_SFl_Corr = new TF1("CSVM_SFl_Corr","(1.10422 + (-0.000523856*x) + (1.14251e-06*(x*x)))", 0.,670.);
+  CSVM_SFl_0to0p8_max =   new TF1("CSVM_SFl_0to0p8_max","((1.15201+(0.00292575*x))+(-7.41497e-06*(x*x)))+(5.0512e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_0p8to1p6_max = new TF1("CSVM_SFl_0p8to1p6_max","((1.17735+(0.00156533*x))+(-4.32257e-06*(x*x)))+(3.18197e-09*(x*(x*x)))", 20.,800.);
+  CSVM_SFl_1p6to2p4_max = new TF1("CSVM_SFl_1p6to2p4_max","((1.17671+(0.0010147*x))+(-3.66269e-06*(x*x)))+(2.88425e-09*(x*(x*x)))", 20.,700.);
 
   edm::FileInPath fipEffMap(effMapFile_);
 
@@ -140,9 +134,9 @@ BTaggingSFProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
 
   BTagSFUtil btaggingUtility( runNumber+eventNumber );
 
+  // b-tagging scale factors are applied following the BTV POG recommendations (https://twiki.cern.ch/twiki/bin/viewauth/CMS/BtagPOG)
   for ( std::vector<pat::Jet>::iterator jetIt = jetsBtags->begin(); jetIt != jetsBtags->end(); ++jetIt )
   {
-    // Get the mistag efficiency and the light and heavy flavour tagging scale factors
     double ScaleFactor;
     double ScaleFactor_up;
     double ScaleFactor_down;
@@ -162,14 +156,14 @@ BTaggingSFProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     if ( abs(parton) == 5 )
     {
       double jetPt = jetIt->pt();
-      if ( jetPt>670. ) jetPt = 670.;
-      else if ( jetPt<30. ) jetPt = 30.;
+      if ( jetPt>800. ) jetPt = 800.;
+      else if ( jetPt<20. ) jetPt = 20.;
 
       ScaleFactor     = CSVM_SFb_0to2p4->Eval(jetPt);
       ScaleFactor_unc = CSVM_SFb_errors->GetBinContent(CSVM_SFb_errors->GetXaxis()->FindBin(jetIt->pt()));
 
-      ScaleFactor_up   = ScaleFactor + SFb_Unc_MultFactor*ScaleFactor_unc;
-      ScaleFactor_down = ScaleFactor - SFb_Unc_MultFactor*ScaleFactor_unc;
+      ScaleFactor_up   = ScaleFactor + ScaleFactor_unc;
+      ScaleFactor_down = ScaleFactor - ScaleFactor_unc;
 
       tagEfficiency = h2_EffMapB->GetBinContent( h2_EffMapB->GetXaxis()->FindBin(jetIt->pt()), h2_EffMapB->GetYaxis()->FindBin(fabs(jetIt->eta())) );
     }
@@ -177,51 +171,43 @@ BTaggingSFProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
     else if ( abs(parton) == 4 )
     {
       double jetPt = jetIt->pt();
-      if ( jetPt>670. ) jetPt = 670.;
-      else if ( jetPt<30. ) jetPt = 30.;
+      if ( jetPt>800. ) jetPt = 800.;
+      else if ( jetPt<20. ) jetPt = 20.;
 
       ScaleFactor     = CSVM_SFb_0to2p4->Eval(jetPt);
       ScaleFactor_unc = CSVM_SFb_errors->GetBinContent(CSVM_SFb_errors->GetXaxis()->FindBin(jetIt->pt()));
 
-      ScaleFactor_up   = ScaleFactor + 2*SFb_Unc_MultFactor*ScaleFactor_unc;
-      ScaleFactor_down = ScaleFactor - 2*SFb_Unc_MultFactor*ScaleFactor_unc;
+      ScaleFactor_up   = ScaleFactor + 2*ScaleFactor_unc;
+      ScaleFactor_down = ScaleFactor - 2*ScaleFactor_unc;
 
       tagEfficiency = h2_EffMapC->GetBinContent( h2_EffMapC->GetXaxis()->FindBin(jetIt->pt()), h2_EffMapC->GetYaxis()->FindBin(fabs(jetIt->eta())) );
     }
+    // if light flavor
     else
     {
       double jetPt = jetIt->pt();
-      if ( jetPt>670. ) jetPt = 670.;
-      else if ( jetPt<20. ) jetPt = 20.;
       double jetAbsEta = fabs(jetIt->eta());
+      if ( jetAbsEta<1.6 && jetPt>800. ) jetPt = 800.;
+      else if ( jetAbsEta>=1.6 && jetPt>700. ) jetPt = 700.;
+      else if ( jetPt<20. ) jetPt = 20.;
 
-      // here, we need to keep the relative uncertainties unchanged
-      if( jetIt->pt()>670 )
+      if(jetAbsEta<0.8)
       {
-	ScaleFactor      = CSVM_SFl_Corr->Eval(670)*CSVM_SFl_0to2p4->Eval(670);
-	ScaleFactor_up   = ScaleFactor + 2*( (CSVM_SFl_0to2p4_max->Eval(670) - CSVM_SFl_0to2p4->Eval(670))/CSVM_SFl_0to2p4->Eval(670) )*ScaleFactor;
-        ScaleFactor_down = ScaleFactor + 2*( (CSVM_SFl_0to2p4_min->Eval(670) - CSVM_SFl_0to2p4->Eval(670))/CSVM_SFl_0to2p4->Eval(670) )*ScaleFactor;
+        ScaleFactor      = CSVM_SFl_0to0p8->Eval(jetPt);
+        ScaleFactor_up   = ScaleFactor + ( jetIt->pt()>800. ? 2. : 1. )*(CSVM_SFl_0to0p8_max->Eval(jetPt) - CSVM_SFl_0to0p8->Eval(jetPt));
+        ScaleFactor_down = ScaleFactor + ( jetIt->pt()>800. ? 2. : 1. )*(CSVM_SFl_0to0p8_min->Eval(jetPt) - CSVM_SFl_0to0p8->Eval(jetPt));
+      }
+      else if(jetAbsEta>=0.8 && jetAbsEta<1.6)
+      {
+        ScaleFactor      = CSVM_SFl_0p8to1p6->Eval(jetPt);
+        ScaleFactor_up   = ScaleFactor + ( jetIt->pt()>800. ? 2. : 1. )*(CSVM_SFl_0p8to1p6_max->Eval(jetPt) - CSVM_SFl_0p8to1p6->Eval(jetPt));
+        ScaleFactor_down = ScaleFactor + ( jetIt->pt()>800. ? 2. : 1. )*(CSVM_SFl_0p8to1p6_min->Eval(jetPt) - CSVM_SFl_0p8to1p6->Eval(jetPt));
       }
       else
       {
-	if(jetAbsEta<0.8)
-	{
-	  ScaleFactor      = CSVM_SFl_Corr->Eval(jetPt)*CSVM_SFl_0to0p8->Eval(jetPt);
-	  ScaleFactor_up   = ScaleFactor + ( (CSVM_SFl_0to0p8_max->Eval(jetPt) - CSVM_SFl_0to0p8->Eval(jetPt))/CSVM_SFl_0to0p8->Eval(jetPt) )*ScaleFactor;
-	  ScaleFactor_down = ScaleFactor + ( (CSVM_SFl_0to0p8_min->Eval(jetPt) - CSVM_SFl_0to0p8->Eval(jetPt))/CSVM_SFl_0to0p8->Eval(jetPt) )*ScaleFactor;
-	}
-	else if(jetAbsEta>=0.8 && jetAbsEta<1.6)
-	{
-	  ScaleFactor      = CSVM_SFl_Corr->Eval(jetPt)*CSVM_SFl_0p8to1p6->Eval(jetPt);
-	  ScaleFactor_up   = ScaleFactor + ( (CSVM_SFl_0p8to1p6_max->Eval(jetPt) - CSVM_SFl_0p8to1p6->Eval(jetPt))/CSVM_SFl_0p8to1p6->Eval(jetPt) )*ScaleFactor;
-	  ScaleFactor_down = ScaleFactor + ( (CSVM_SFl_0p8to1p6_min->Eval(jetPt) - CSVM_SFl_0p8to1p6->Eval(jetPt))/CSVM_SFl_0p8to1p6->Eval(jetPt) )*ScaleFactor;
-	}
-	else
-	{
-	  ScaleFactor      = CSVM_SFl_Corr->Eval(jetPt)*CSVM_SFl_1p6to2p4->Eval(jetPt);
-	  ScaleFactor_up   = ScaleFactor + ( (CSVM_SFl_1p6to2p4_max->Eval(jetPt) - CSVM_SFl_1p6to2p4->Eval(jetPt))/CSVM_SFl_1p6to2p4->Eval(jetPt) )*ScaleFactor;
-	  ScaleFactor_down = ScaleFactor + ( (CSVM_SFl_1p6to2p4_min->Eval(jetPt) - CSVM_SFl_1p6to2p4->Eval(jetPt))/CSVM_SFl_1p6to2p4->Eval(jetPt) )*ScaleFactor;
-	}
+        ScaleFactor      = CSVM_SFl_1p6to2p4->Eval(jetPt);
+        ScaleFactor_up   = ScaleFactor + ( jetIt->pt()>700. ? 2. : 1. )*(CSVM_SFl_1p6to2p4_max->Eval(jetPt) - CSVM_SFl_1p6to2p4->Eval(jetPt));
+        ScaleFactor_down = ScaleFactor + ( jetIt->pt()>700. ? 2. : 1. )*(CSVM_SFl_1p6to2p4_min->Eval(jetPt) - CSVM_SFl_1p6to2p4->Eval(jetPt));
       }
 
       tagEfficiency = h2_EffMapUDSG->GetBinContent( h2_EffMapUDSG->GetXaxis()->FindBin(jetIt->pt()), h2_EffMapUDSG->GetYaxis()->FindBin(fabs(jetIt->eta())) );
