9e0db6742851f6f0a9ba1262e8c840ad9d047a01
skhalil <>
2012-12-29 21:50:06 +0000

    new JEC payloads

--- EDSHyFT/test/dumpJEC_53x.py
+++ EDSHyFT/test/dumpJEC_53x.py
@@ -6,6 +6,31 @@ process.maxEvents = cms.untracked.PSet(
         input = cms.untracked.int32(1)
         )
 
+###############################
+####### Parameters ############
+###############################
+from FWCore.ParameterSet.VarParsing import VarParsing
+options = VarParsing ('python')
+
+options.register ('runData',
+                  0,
+                  VarParsing.multiplicity.singleton,
+                  VarParsing.varType.int,
+                  "GT for data (1) or MC (0)")
+
+options.parseArguments()
+print options
+
+GTData = ''
+OutSuffix = ''
+
+if options.runData==1:
+	GTData = 'GR_P_V39_AN3::All'
+        OutSuffix = 'Jec12_V3'
+else: 
+	GTData = 'MC_53_V15::All'
+        OutSuffix = 'Jec12_V3_MC'
+
 process.source = cms.Source("EmptySource")
 
 process.load('Configuration.StandardSequences.Services_cff')
@@ -15,7 +40,7 @@ from JetMETCorrections.Configuration.JetCorrectionServicesAllAlgos_cff import *
 
 ## Geometry and Detector Conditions (needed for a few patTuple production steps)
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string( 'FT_53_V10_AN3::All' )
+process.GlobalTag.globaltag = cms.string( GTData )
 
 ##-------------------- Import the JEC services -----------------------
 process.load('JetMETCorrections.Configuration.DefaultJEC_cff')
@@ -23,6 +48,6 @@ process.readAK5PFchs = cms.EDAnalyzer('JetCorrectorDBReader',
                                    payloadName    = cms.untracked.string('AK5PFchs'),
                                    printScreen    = cms.untracked.bool(False),
                                    createTextFile = cms.untracked.bool(True),
-                                   globalTag      = cms.untracked.string('Jec12_V3')
+                                   globalTag      = cms.untracked.string(OutSuffix)
                                    )
 process.p = cms.Path( process.readAK5PFchs )
