0f38b8b8baa4fe2b2c0f230071dbbc8a5ee5b345
ferencek <>
2013-01-26 04:55:33 +0000

    improved handling of overflow bins, added more datasets

--- EDSHyFT/test/bTaggingEfficiency/makeBTaggingEfficiencyMapAK5PF.py
+++ EDSHyFT/test/bTaggingEfficiency/makeBTaggingEfficiencyMapAK5PF.py
@@ -109,6 +109,160 @@ datasets = [
      'udsg': [[0., 40., 60., 80., 100., 150., 200., 1000.],[0., 0.6, 1.2, 2.4]]},
     'AK5PF_CSVM'
   ],
+  [
+    '/QCD_Pt-20to30_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-30to50_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-50to80_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt-50to80_MuEnrichedPt5_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-80to120_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 140., 180., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 140., 180., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 140., 180., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-120to170_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt-120to170_MuEnrichedPt5_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-170to300_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt-170to300_MuEnrichedPt5_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-300to470_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt-300to470_MuEnrichedPt5_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 550., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 550., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 550., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-470to600_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt-470to600_MuEnrichedPt5_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 500., 600., 700., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 500., 600., 700., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 500., 600., 700., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-600to800_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt-800to1000_MuEnrichedPt5_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 950., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 950., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 950., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_20_30_EMEnriched_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt_30_80_EMEnriched_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_80_170_EMEnriched_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_170_250_EMEnriched_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_250_350_EMEnriched_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 400., 450., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_350_EMEnriched_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_20_30_BCtoE_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_30_80_BCtoE_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 120., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_80_170_BCtoE_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt_80_170_BCtoE_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_170_250_BCtoE_TuneZ2star_8TeV_pythia6/cjenkins-QCD_Pt_170_250_BCtoE_TuneZ2star_8TeV_pythia6-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 350., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 300., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/QCD_Pt_350_BCtoE_TuneZ2star_8TeV_pythia6/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v2_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 800., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 300., 400., 500., 600., 700., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
+  [
+    '/GJets_HT-200To400_8TeV-madgraph/StoreResults-Summer12_DR53X-PU_S10_START53_V7A-v1_TLBSM_53x_v2-c04f3b4fa74c8266c913b71e0c74901d/USER',
+    {'b':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'c':    [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]],
+     'udsg': [[0., 40., 60., 80., 100., 150., 200., 250., 1000.],[0., 0.6, 1.2, 2.4]]},
+    'AK5PF_CSVM'
+  ],
   # Signal
   # BprimeBprimeToBHBHinc
   [
@@ -770,29 +924,35 @@ def produceEfficiencyMaps(dataset, inputPath, subdirectory, suffix):
     for i in range(1,denominatorOut.GetXaxis().GetNbins()+1):
       for j in range(1,denominatorOut.GetYaxis().GetNbins()+1):
 
-	binXMin = denominatorIn.GetXaxis().FindBin(denominatorOut.GetXaxis().GetBinLowEdge(i)+xShift)
-	binXMax = denominatorIn.GetXaxis().FindBin(denominatorOut.GetXaxis().GetBinUpEdge(i)-xShift)
-	binYMinPos = denominatorIn.GetYaxis().FindBin(denominatorOut.GetYaxis().GetBinLowEdge(j)+yShift)
-	binYMaxPos = denominatorIn.GetYaxis().FindBin(denominatorOut.GetYaxis().GetBinUpEdge(j)-yShift)
-	binYMinNeg = denominatorIn.GetYaxis().FindBin(-denominatorOut.GetYaxis().GetBinUpEdge(j)+yShift)
-	binYMaxNeg = denominatorIn.GetYaxis().FindBin(-denominatorOut.GetYaxis().GetBinLowEdge(j)-yShift)
+        binXMin = denominatorIn.GetXaxis().FindBin(denominatorOut.GetXaxis().GetBinLowEdge(i)+xShift)
+        binXMax = denominatorIn.GetXaxis().FindBin(denominatorOut.GetXaxis().GetBinUpEdge(i)-xShift)
+        binYMinPos = denominatorIn.GetYaxis().FindBin(denominatorOut.GetYaxis().GetBinLowEdge(j)+yShift)
+        binYMaxPos = denominatorIn.GetYaxis().FindBin(denominatorOut.GetYaxis().GetBinUpEdge(j)-yShift)
+        binYMinNeg = denominatorIn.GetYaxis().FindBin(-denominatorOut.GetYaxis().GetBinUpEdge(j)+yShift)
+        binYMaxNeg = denominatorIn.GetYaxis().FindBin(-denominatorOut.GetYaxis().GetBinLowEdge(j)-yShift)
 
-	denominator = denominatorIn.Integral(binXMin,binXMax,binYMinPos,binYMaxPos)
-	denominator = denominator + denominatorIn.Integral(binXMin,binXMax,binYMinNeg,binYMaxNeg)
-	numerator = numeratorIn.Integral(binXMin,binXMax,binYMinPos,binYMaxPos)
-	numerator = numerator + numeratorIn.Integral(binXMin,binXMax,binYMinNeg,binYMaxNeg)
+        denominator = denominatorIn.Integral(binXMin,binXMax,binYMinPos,binYMaxPos)
+        denominator = denominator + denominatorIn.Integral(binXMin,binXMax,binYMinNeg,binYMaxNeg)
+        numerator = numeratorIn.Integral(binXMin,binXMax,binYMinPos,binYMaxPos)
+        numerator = numerator + numeratorIn.Integral(binXMin,binXMax,binYMinNeg,binYMaxNeg)
 
-	denominatorOut.SetBinContent(i,j,denominator)
-	numeratorOut.SetBinContent(i,j,numerator)
-	if(denominator>0.): efficiencyOut.SetBinContent(i,j,numerator/denominator)
+        if(i==denominatorOut.GetXaxis().GetNbins()): # also add overflow to the last bin in jet pT
+          denominator = denominator + denominatorIn.Integral(binXMax+1,denominatorIn.GetXaxis().GetNbins()+1,binYMinPos,binYMaxPos)
+          denominator = denominator + denominatorIn.Integral(binXMax+1,denominatorIn.GetXaxis().GetNbins()+1,binYMinNeg,binYMaxNeg)
+          numerator = numerator + numeratorIn.Integral(binXMax+1,numeratorIn.GetXaxis().GetNbins()+1,binYMinPos,binYMaxPos)
+          numerator = numerator + numeratorIn.Integral(binXMax+1,numeratorIn.GetXaxis().GetNbins()+1,binYMinNeg,binYMaxNeg)
+
+        denominatorOut.SetBinContent(i,j,denominator)
+        numeratorOut.SetBinContent(i,j,numerator)
+        if(denominator>0.): efficiencyOut.SetBinContent(i,j,numerator/denominator)
 
     # check if there are any bins with 0 or 100% efficiency
     for i in range(1,denominatorOut.GetXaxis().GetNbins()+1):
       for j in range(1,denominatorOut.GetYaxis().GetNbins()+1):
 
-	efficiency = efficiencyOut.GetBinContent(i,j)
-	if(efficiency==0. or efficiency==1.):
-	  print 'Warning! Bin(%i,%i) for %s jets has a b-tagging efficiency of %.3f'%(i,j,partonFlavor,efficiency)
+        efficiency = efficiencyOut.GetBinContent(i,j)
+        if(efficiency==0. or efficiency==1.):
+          print 'Warning! Bin(%i,%i) for %s jets has a b-tagging efficiency of %.3f'%(i,j,partonFlavor,efficiency)
 
     # set efficiencies in overflow bins
     for i in range(1,denominatorOut.GetXaxis().GetNbins()+1):
