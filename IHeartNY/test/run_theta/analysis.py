907f3a24fc57624074a5bf1d601e2c2a809d88ca
Salvatore Rappoccio <rappoccio@gmail.com>
2014-06-09 17:38:49 -0500

    Adding a theta model that can externalize PDF and Q2 uncertainties

--- IHeartNY/test/run_theta/analysis.py
+++ IHeartNY/test/run_theta/analysis.py
@@ -1,10 +1,113 @@
 from ROOT import *
 
-def histfilter( hname ) :
-    return True
+####################################################################################
+# We have to externalize the PDF and Q2 uncertainties.
+# This is done by skipping them in the nominal variations,
+# but then re-running with "nominal" set to "PDF up", "PDF down", etc, respectively.
+# Thus, for each running (nominal, pdfup, pdfdown, scaleup, scaledown), there is
+# a histogram filter to get rid of unwanted bits, and a histname modifier that
+# changes the name to the appropriate one when recentering on the externalized
+# "up" and "down" variations. 
+####################################################################################
 
-def muplusjets(files, infilter, signal, mcstat):
-    model = build_model_from_rootfile(files, histogram_filter=infilter, include_mc_uncertainties = mcstat)
+def histfilter( hname ) :
+    if hname == None or 'qcd' in hname or 'pdf' in hname or 'scale' in hname :
+        return False
+    else :
+        return True
+
+def pdf_up_histfilter( hname ) :
+    isQCD = 'qcd' in hname
+    isScaleUp = 'scale__up' in hname
+    isPDFUp = 'pdf__up' in hname
+    isScaleDown = 'scale__down' in hname
+    isPDFDown = 'pdf__down' in hname
+    isTTbarNonSemiLepNominal = ('TTbar_nonSemiLep' in hname and 'TTbar_nonSemiLep__' not in hname)
+    isTTbarNominal = ('nonSemiLep' not in hname and 'TTbar' in hname and 'TTbar__' not in hname)
+    if hname == None or isQCD or isScaleUp or isScaleDown or isPDFDown or isTTbarNonSemiLepNominal or isTTbarNominal :
+        return False
+    else :
+        return True
+
+def pdf_down_histfilter( hname ) :
+    isQCD = 'qcd' in hname
+    isScaleUp = 'scale__up' in hname
+    isPDFUp = 'pdf__up' in hname
+    isScaleDown = 'scale__down' in hname
+    isPDFDown = 'pdf__down' in hname
+    isTTbarNonSemiLepNominal = ('TTbar_nonSemiLep' in hname and 'TTbar_nonSemiLep__' not in hname)
+    isTTbarNominal = ('nonSemiLep' not in hname and 'TTbar' in hname and 'TTbar__' not in hname)
+    if hname == None or isQCD or isScaleUp or isScaleDown or isPDFUp or isTTbarNonSemiLepNominal or isTTbarNominal :
+        return False
+    else :
+        return True
+
+
+
+def scale_up_histfilter( hname ) :
+    isQCD = 'qcd' in hname
+    isScaleUp = 'scale__up' in hname
+    isPDFUp = 'pdf__up' in hname
+    isScaleDown = 'scale__down' in hname
+    isPDFDown = 'pdf__down' in hname
+    isTTbarNonSemiLepNominal = ('TTbar_nonSemiLep' in hname and 'TTbar_nonSemiLep__' not in hname)
+    isTTbarNominal = ('nonSemiLep' not in hname and 'TTbar' in hname and 'TTbar__' not in hname)
+    if hname == None or isQCD  or isScaleDown or isPDFUp or isPDFDown or isTTbarNonSemiLepNominal or isTTbarNominal :
+        return False
+    else :
+        return True
+
+def scale_down_histfilter( hname ) :
+    isQCD = 'qcd' in hname
+    isScaleUp = 'scale__up' in hname
+    isPDFUp = 'pdf__up' in hname
+    isScaleDown = 'scale__down' in hname
+    isPDFDown = 'pdf__down' in hname
+    isTTbarNonSemiLepNominal = ('TTbar_nonSemiLep' in hname and 'TTbar_nonSemiLep__' not in hname)
+    isTTbarNominal = ('nonSemiLep' not in hname and 'TTbar' in hname and 'TTbar__' not in hname)
+    if hname == None or isQCD or isScaleUp or isPDFUp or isPDFDown or isTTbarNonSemiLepNominal or isTTbarNominal :
+        return False
+    else :
+        return True
+
+        
+def pdf_unc_up_modifier( hname ) :
+    if 'pdf__up' in hname :
+        outhname = hname.replace( '__pdf__up', '')
+        return outhname
+    else :
+        return hname
+
+def pdf_unc_down_modifier( hname ) :
+    if 'pdf__down' in hname :
+        outhname = hname.replace( '__pdf__down', '')
+        return outhname
+    else :
+        return hname
+
+def scale_unc_up_modifier( hname ) :
+    if 'scale__up' in hname :
+        outhname = hname.replace( '__scale__up', '')
+        return outhname
+    else :
+        return hname
+
+def scale_unc_down_modifier( hname ) :
+    if 'scale__down' in hname :
+        outhname = hname.replace( '__scale__down', '')
+        return outhname
+    else :
+        return hname
+
+
+####################################################################################
+# Here is where we build the model for theta
+####################################################################################
+def muplusjets(files, infilter, signal, mcstat, ex_to_in):
+    if ex_to_in != None : 
+        model = build_model_from_rootfile(files, histogram_filter=infilter, include_mc_uncertainties = mcstat, root_hname_to_convention = ex_to_in)
+    else :
+        model = build_model_from_rootfile(files, histogram_filter=infilter, include_mc_uncertainties = mcstat)
     model.fill_histogram_zerobins()
     model.set_signal_processes(signal)
     for p in model.processes:
@@ -13,8 +116,11 @@ def muplusjets(files, infilter, signal, mcstat):
 
 
     #model.add_lognormal_uncertainty('ttbar_rate', math.log(1.15), 'ttbar')
-    model.add_lognormal_uncertainty('st_rate', math.log(1.5), 'SingleTop')
+    model.add_lognormal_uncertainty('rate_st', math.log(1.5), 'SingleTop')
     model.add_lognormal_uncertainty('rate_vjets', math.log(1.5), 'WJets')
+    model.add_lognormal_uncertainty('rate_nonsemi', math.log(1.5), 'TTbar_nonSemiLep')
+    #model.add_lognormal_uncertainty('rate_qcd', math.log(1.5), 'QCD')
+
     #    model.add_asymmetric_lognormal_uncertainty('scale_vjets', -math.log(1.577), math.log(0.710), 'WJets', obs)
     #    model.add_asymmetric_lognormal_uncertainty('matching_vjets', -math.log(1.104), math.log(1.052), 'WJets', obs)
 
@@ -48,34 +154,32 @@ def muplusjets(files, infilter, signal, mcstat):
 
 import exceptions
 
-
-def build_model(type, jet1 = None, mcstat = True):
-
-    print '-------------------------'
-    print 'Hello from build_model!'
-    print '-------------------------'
-
+####################################################################################
+# Here is where the constructed model is declared to theta
+####################################################################################
+def build_model(type, jet1 = None, mcstat = True, ex_to_in = None, infilter = None):
     model = None
 
     if type == 'ttbar_xs' :
 
         model = muplusjets(
             files=[#'normalized_mujets_ptMET3_subtracted_from_ptMET1.root', 'normalized_mujets_ptMET5_subtracted_from_ptMET3.root',
-                   #'normalized_mujets_vtxMass6_subtracted_from_vtxMass5.root',
-                   'normalized_mujets_vtxMass6.root'],
-            infilter=histfilter,
+                    #'normalized_mujets_ht7_subtracted_from_ht4.root',
+                    'normalized_mujets_vtxMass7.root'],
+            infilter=infilter,
             signal='TTbar',
-            mcstat=mcstat
+            mcstat=mcstat,
+            ex_to_in=ex_to_in
         )
 
     else:
 
         raise exceptions.ValueError('Type %s is undefined' % type)
 
-    for p in model.distribution.get_parameters():
-        d = model.distribution.get_distribution(p)
-        if d['typ'] == 'gauss' and d['mean'] == 0.0 and d['width'] == 1.0:
-            model.distribution.set_distribution_parameters(p, range = [-5.0, 5.0])
+    #for p in model.distribution.get_parameters():
+    #    d = model.distribution.get_distribution(p)
+    #    if d['typ'] == 'gauss' and d['mean'] == 0.0 and d['width'] == 1.0:
+    #        model.distribution.set_distribution_parameters(p, range = [-5.0, 5.0])
         #if 'rate' in p:
         #    if d['typ'] == 'gauss' and d['mean'] == 0.0 and d['width'] == 1.0:
         #        model.distribution.set_distribution_parameters(p, range = [-5.0, 5.0])
@@ -83,44 +187,97 @@ def build_model(type, jet1 = None, mcstat = True):
         #    if d['typ'] == 'gauss' and d['mean'] == 0.0 and d['width'] == 1.0:
         #        model.distribution.set_distribution_parameters(p, range = [-0.0, 0.0])
 
-    print model
     return model
 
 
 
-# Code introduced by theta_driver
+####################################################################################
+# Here is the "main" part of the script. 
+####################################################################################
 
-# Building the statistical model
-args = {'type': 'ttbar_xs'}
+# Building the statistical model :
+# These are the bits that need for externalizing pdf and q2 uncertainties.
+# Thus, we run nominal, pdfup, pdfdown, q2up, q2 down separately. 
+filters = [histfilter, pdf_up_histfilter, pdf_down_histfilter, scale_up_histfilter, scale_down_histfilter]
+ex_to_in_variations = [None, pdf_unc_up_modifier, pdf_unc_down_modifier, scale_unc_up_modifier, scale_unc_down_modifier ]
+ex_to_in_names = ['Nominal', 'pdfup', 'pdfdown', 'scaleup', 'scaledown']
 
-model = build_model(**args)
+for iex_to_in_variation in xrange( len(ex_to_in_variations) ) :
+    ex_to_in_variation = ex_to_in_variations[ iex_to_in_variation ]
+    ex_to_in_name = ex_to_in_names[ iex_to_in_variation ]
+    infilter = filters[ iex_to_in_variation ]
 
+    args = {'type': 'ttbar_xs',
+            'mcstat':False,
+            'ex_to_in':ex_to_in_variation,
+            'infilter':infilter}
 
-model_summary(model, all_nominal_templates=True, shape_templates=True) 
+    model = build_model(**args)
 
-parameters = model.get_parameters(['TTbar'])
-print parameters
+    #model_summary(model, all_nominal_templates=True, shape_templates=True) 
 
-print '------------- MLE RESULTS ---------------'
+    parameters = model.get_parameters(['TTbar'])
 
-args = {}
+    print '------------- MLE RESULTS ' + ex_to_in_name + ' ---------------'
 
-results1 = mle(model, input='toys:1.', n=10 , with_covariance=True, **args)
-results2 = mle(model, input='data', n=1 , with_covariance=True, **args)
-
-print results2
-
-
-print '------------- PL RESULTS ---------------'
-
-
-args = {}
-
-results3 = pl_interval(model, input='toys:1.', n=10 ,  **args)
-results4 = pl_interval(model, input='data', n=1 , **args)
-
-print results4
+    results1 = mle(model, input='toys:1.', n=1000)
 
 
+    bs = []
+    delta_bs = []
+    pulls = []
 
-report.write_html('htmlout')
+    for b, db in results1['TTbar']['beta_signal']:
+        bs.append(b)
+        delta_bs.append(db)
+        pulls.append((1 - b)/db)
+
+    pdbs = plotdata()
+    pdbs.histogram(bs, 0.0, 2.0, 30, include_uoflow = True)
+    plot(pdbs, 'bs', 'ntoys', 'beta_signal_' + ex_to_in_name + '.pdf')
+
+    pdd = plotdata()
+    pdd.histogram(delta_bs, 0.0, 1.0, 30, include_uoflow = True)
+    plot(pdd, 'dbs', 'ntoys', 'delta_beta_signal_' + ex_to_in_name + '.pdf')
+
+    pdp = plotdata()
+    pdp.histogram(pulls, -3.0, 3.0, 100, include_uoflow = True)
+    plot(pdp, 'pull', 'ntoys', 'pull_' + ex_to_in_name + '.pdf')
+
+
+    # to write the data to a file, use e.g.:
+    pdp.write_txt('pull_' + ex_to_in_name + '.txt')
+
+    # to write it to a root file:
+    write_histograms_to_rootfile({'pull': pdp.histo(), 'bs': pdbs.histo(), 'delta_bs': pdd.histo()}, 'pulldists_mle_' + ex_to_in_name + '.root')
+
+
+
+
+
+    results2 = mle(model, input='data', n=1)
+
+
+    ## parameter_values = {}
+    ## for p in model.get_parameters([]):
+    ##     parameter_values[p] = results2['TTbar'][p][0][0]
+    ## histos = evaluate_prediction(model, parameter_values, include_signal = True)
+    ## write_histograms_to_rootfile(histos, 'histos_mle.root')
+
+
+    print results2
+
+
+    #print '------------- PL RESULTS ---------------'
+
+
+    #args = {}
+
+    #results3 = pl_interval(model, input='toys:1.', n=1000 ,  **args)
+    #results4 = pl_interval(model, input='data', n=1 , **args)
+
+    #print results4
+
+
+    if ex_to_in_name == "Nominal" : 
+        report.write_html('htmlout_' + ex_to_in_name)
