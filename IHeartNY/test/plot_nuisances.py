fc3921f134d9b2b5a9f660cbfa34304328382560
Salvatore Rappoccio <rappoccio@gmail.com>
2014-06-27 10:59:39 +0200

    Adding a plot for the MLE nuisance parameters from Justin

---
+++ IHeartNY/test/plot_nuisances.py
@@ -0,0 +1,76 @@
+from ROOT import *
+
+ttbar_list = {'TTbar': {'jer': [(0.33256440316996799, 0.5213602052500288)], 'toptag': [(-0.19080012970727078, 0.51727816842157792)], 'btag': [(-0.051541564184084135, 1.9403431902448474)], 'rate_nonsemi': [(0.01828532985966105, 1.0153030722733016)], 'beta_signal': [(0.90868829145921137, 0.13536619569539265)], 'jec': [(0.18903575258490485, 0.42614560289944264)], 'rate_vjets': [(-0.5271805541639909, 0.21792895617772823)], '__nll': [-36018.106725770893], 'lumi': [(-0.013007155068815879, 1.00629216883878)], 'rate_st': [(-0.13192106969623793, 0.97997424812413847)]}}
+
+signal_list = ttbar_list['TTbar']
+
+nuisances = TGraphErrors()
+gr1sig = TGraphErrors()
+gr2sig = TGraphErrors()
+
+nPoint = -1
+for systematic in sorted(signal_list.keys()):
+
+	if systematic is '__nll':
+		continue
+
+	nPoint += 1
+
+	print systematic, signal_list[systematic][0][0], signal_list[systematic][0][1]
+
+
+	nuisances.SetPoint(nPoint, signal_list[systematic][0][0], nPoint+0.5)
+	nuisances.SetPointError(nPoint, signal_list[systematic][0][1], 0)
+
+	gr1sig.SetPoint(nPoint, 0, nPoint)
+	gr1sig.SetPointError(nPoint, 1, 1)
+	
+	gr2sig.SetPoint(nPoint, 0, nPoint)
+	gr2sig.SetPointError(nPoint, 2, 1)
+	
+
+
+
+
+
+c1 = TCanvas("c1", "c1", 600, 800)
+c1.Draw()
+c1.SetLeftMargin(0.15)
+
+gr2sig.SetLineWidth(0)
+gr1sig.SetLineWidth(0)
+gr2sig.SetLineColor(kYellow)
+gr1sig.SetLineColor(kGreen)
+gr2sig.SetFillStyle(1001)
+gr2sig.SetFillColor(kYellow)
+
+gr2sig.Draw("A5")
+gr2sig.GetYaxis().Set(nPoint+1, 0, nPoint+1)
+gr2sig.GetXaxis().Set(10,-4.5,4.5)
+
+nBin = 0
+for systematic in sorted(signal_list.keys()):
+	if systematic is '__nll':
+		continue
+	gr2sig.GetYaxis().SetBinLabel(nBin+1, systematic)
+	nBin += 1
+
+gr2sig.GetXaxis().SetTitle("Post-Fit Nuisance Parameter Value (#sigma)")
+gr2sig.GetYaxis().SetTitle("Nuisance Parameter")
+gr2sig.GetYaxis().SetTitleOffset(3)
+
+gr1sig.SetFillStyle(1001)
+gr1sig.SetFillColor(kGreen)
+gr1sig.Draw("5 same")
+
+nuisances.SetMarkerStyle(21)
+nuisances.Draw("P same")
+gPad.RedrawAxis()
+
+label = TLatex()
+label.SetNDC(1)
+
+c1.Print("nuisances.pdf")
+c1.Print("nuisances.png")
+
+
