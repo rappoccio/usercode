452678b99d30791cae713e8e0eb12f649fd24ec9
Louise Skinnari <louise.skinnari@cern.ch>
2014-11-12 17:56:57 +0100

    normalize response matrix

--- IHeartNY/test/unfoldTopPt.py
+++ IHeartNY/test/unfoldTopPt.py
@@ -227,57 +227,6 @@ fout = TFile("UnfoldingPlots/unfold_"+options.syst+".root","recreate");
 
 
 # -------------------------------------------------------------------------------------
-# plot response matrices 
-# -------------------------------------------------------------------------------------
-
-gStyle.SetPadRightMargin(0.12);
-cr = TCanvas("c_response", "", 800, 600)
-
-hEmpty2D = response.Hresponse().Clone()
-hEmpty2D.SetName("empty2D")
-hEmpty2D.Reset()
-hEmpty2D.GetXaxis().SetTitle("Measured top-jet p_{T} [GeV]")
-hEmpty2D.GetYaxis().SetTitle("Top quark p_{T} [GeV]")
-hEmpty2D.GetXaxis().SetLabelSize(0.045)
-hEmpty2D.GetYaxis().SetLabelSize(0.045)
-hEmpty2D.Draw()
-hResponse2D = response.Hresponse().Clone()
-hResponse2D.SetName("plottedResponse")
-
-# normalize so that for each bin of true top quark pt, the bins in measured top pt add up to 100%
-#nbinsX = hResponse2D.GetNbinsX()
-#nbinsY = hResponse2D.GetNbinsX()
-#print "nbr bins in x = " + str(nbinsX) + "nbr bins in y = " + str(nbinsY)
-#for iby in range(1,nbinsY) :
-#    rowIntegral = hResponse2D.Integral(1,nbinsX,iby,iby)
-#    print "for y-bin " + str(iby) + " row integral = " + str(rowIntegral)
-#    for ibx in range(1,nbinsX+1) :
-#        binContent = hResponse2D.GetBinContent(ibx,iby)
-#        newContent = 0
-#        if rowIntegral > 0:
-#            newContent = binContent/rowIntegral*100.0
-#        #print "bin content x-bin " + str(ibx) + " y-bin " + str(iby) + " binContent " + str(binContent) + " newContent " + str(newContent)
-#        hResponse2D.SetBinContent(ibx,iby,newContent)
-
-
-gStyle.SetPaintTextFormat(".1f")
-#gStyle.SetPalette(1)
-hResponse2D.Draw("colz,same,text")
-hEmpty2D.Draw("axis,same")
-cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_"+options.syst+".png")
-cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_"+options.syst+".eps")
-
-response.Hresponse().SetName("responseMatrix_"+options.syst)
-response.Hresponse().Write()
-
-gPad.SetLogz()
-cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_logz_"+options.syst+".png")
-cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_logz_"+options.syst+".eps")
-
-gStyle.SetPadRightMargin(0.05);
-
-
-# -------------------------------------------------------------------------------------
 # read actual histograms
 # -------------------------------------------------------------------------------------
 
@@ -684,6 +633,58 @@ if options.plotFullRange :
 c1.Print("UnfoldingPlots/unfolded_ttbar_xs"+append+"_"+options.syst+".pdf", "pdf")
 c1.Print("UnfoldingPlots/unfolded_ttbar_xs"+append+"_"+options.syst+".png", "png")
 c1.Print("UnfoldingPlots/unfolded_ttbar_xs"+append+"_"+options.syst+".eps", "eps")
+
+
+
+# -------------------------------------------------------------------------------------
+# plot response matrices 
+# (do this in the end as the normalization otherwise will mess up the unfolding result!)
+# -------------------------------------------------------------------------------------
+
+gStyle.SetPadRightMargin(0.12);
+cr = TCanvas("c_response", "", 800, 600)
+
+hEmpty2D = response.Hresponse().Clone()
+hEmpty2D.SetName("empty2D")
+hEmpty2D.Reset()
+hEmpty2D.GetXaxis().SetTitle("Measured top-jet p_{T} [GeV]")
+hEmpty2D.GetYaxis().SetTitle("Top quark p_{T} [GeV]")
+hEmpty2D.GetXaxis().SetLabelSize(0.045)
+hEmpty2D.GetYaxis().SetLabelSize(0.045)
+hEmpty2D.Draw()
+hResponse2D = response.Hresponse().Clone()
+hResponse2D.SetName("plottedResponse")
+
+# normalize so that for each bin of true top quark pt, the bins in measured top pt add up to 100%
+nbinsX = hResponse2D.GetNbinsX()
+nbinsY = hResponse2D.GetNbinsX()
+print "nbr bins in x = " + str(nbinsX) + "nbr bins in y = " + str(nbinsY)
+for iby in range(1,nbinsY) :
+    rowIntegral = hResponse2D.Integral(1,nbinsX,iby,iby)
+    print "for y-bin " + str(iby) + " row integral = " + str(rowIntegral)
+    for ibx in range(1,nbinsX+1) :
+        binContent = hResponse2D.GetBinContent(ibx,iby)
+        newContent = 0
+        if rowIntegral > 0:
+            newContent = binContent/rowIntegral*100.0
+        #print "bin content x-bin " + str(ibx) + " y-bin " + str(iby) + " binContent " + str(binContent) + " newContent " + str(newContent)
+        hResponse2D.SetBinContent(ibx,iby,newContent)
+
+
+gStyle.SetPaintTextFormat(".1f")
+#gStyle.SetPalette(1)
+hResponse2D.Draw("colz,same,text")
+hEmpty2D.Draw("axis,same")
+cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_"+options.syst+".png")
+cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_"+options.syst+".eps")
+
+response.Hresponse().SetName("responseMatrix_"+options.syst)
+response.Hresponse().Write()
+
+gPad.SetLogz()
+cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_logz_"+options.syst+".png")
+cr.SaveAs("UnfoldingPlots/unfold_responseMatrix_logz_"+options.syst+".eps")
+
 
 
 fout.Close()
