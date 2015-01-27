5395f4e20e2a5aac6e35cb94d763e3eda52e4573
Louise Skinnari <louise.skinnari@cern.ch>
2015-01-27 14:25:56 +0100

    add electron run files

---
+++ IHeartNY/test/utils/run_nontop_el_iheartny.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from run_iheartny import *
+
+import shlex
+
+
+
+samples = [
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/W1JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           title='W1JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           pu='wjets', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/W2JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           title='W2JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           pu='wjets', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/skinnari/TopXS_slc6/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/W3JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           title='W3JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           pu='wjets', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/skinnari/TopXS_slc6/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/W4JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           title='W4JetsToLNu_TuneZ2Star_8TeV-madgraph_iheartNY_V1_el',
+           pu='wjets', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/T_s-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='T_s-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='sts', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/T_t-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='T_t-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='stt', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='T_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='sttw', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='Tbar_s-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='stsb', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='Tbar_t-channel_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='sttb', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\''),
+    Sample(directory='/uscms_data/d3/maral87/TopXS/ntuples_e_012114/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola_iheartNY_el',
+           title='Tbar_tW-channel-DR_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           pu='sttwb', pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True, flags='--lepType=\'ele\'')
+]
+
+# run the different channels
+run_threads( samples )
---
+++ IHeartNY/test/utils/run_pdfs_CT10up_el_iheartny.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+from run_iheartny import run_threads, Sample
+
+import shlex
+
+
+
+samples = [
+    
+    #------------------------------    
+    # PDF Up samples, CT10
+    #------------------------------
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_max700_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--mttGenMax=700. --makeResponse --semilep=1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--makeResponse  --semilep=1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--makeResponse  --semilep=1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_nonSemiLep_max700_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--mttGenMax=700. --semilep=-1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_nonSemiLep_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--semilep=-1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+    Sample(directory='/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el',
+           title='TT_nonSemiLep_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_el_CT10_pdfup',
+           flags='--semilep=-1.0 --pdfSys=1.0 --pdfSet=0.0 --lepType=\'ele\'',
+           pdfsys=False, noms=True, jersys=True, jecsys=True, btagsys=True, toptagsys=True, qcd=True, newiso=True
+    ),
+
+
+]
+
+
+run_threads( samples )
+# Now the threads are done, exit gracefully
+print 'So long, and thanks for all the fish!'
