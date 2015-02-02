218218da800ea14f910129365fc062a083fbfef3
Louise Skinnari <louise.skinnari@cern.ch>
2015-02-02 16:51:41 +0100

    new run scripts

--- IHeartNY/test/TruthStudy/run_truthStuff_CT10.py
+++ IHeartNY/test/TruthStudy/run_truthStuff_CT10.py
@@ -7,18 +7,18 @@ import threading
 
 
 samples = [
-    '--files=/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu/res/*root',
-    '--files=/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu/res/*root',
-    '--files=/uscms_data/d3/sdittmer/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu/res/*root'
+    '--files=/uscms/home/skinnari/nobackup/TopXS_slc6/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_truth_mu/res/*root',
+    '--files=/uscms/home/skinnari/nobackup/TopXS_slc6/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_truth_mu/res/*root',
+    '--files=/uscms/home/skinnari/nobackup/TopXS_slc6/CMSSW_5_3_22_patch1/src/Analysis/IHeartNY/test/TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_truth_mu/res/*root'
 ]
 outname = [
-    '--outname=TT_max700_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu_',
-    '--outname=TT_Mtt-700to1000_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu_',
-    '--outname=TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu_'
+    '--outname=TT_max700_',
+    '--outname=TT_Mtt-700to1000_',
+    '--outname=TT_Mtt-1000toInf_'
 ]
 
 oddeven = [ '--oddeven=1', '--oddeven=2' ]
-name_oddeven = [ 'odd', 'even' ]
+name_oddeven = [ '_odd', '_even' ]
 
 pdfsysts = [ '--pdfSys=0.0', '--pdfSys=1.0', '--pdfSys=-1.0' ]
 name_pdfsysts = [ "_nom", "_pdfup", "_pdfdown" ]
@@ -46,9 +46,9 @@ def run_threads (samples) :
         for ioe in range(0,len(oddeven)):
             for isyst in range(0,len(pdfsysts)):
                 for iset in range(0,len(pdfsets)):
-                    s = [ 'python', 'iheartny_topxs_fwlite.py', samples[isample], oddeven[ioe], pdfsysts[isyst], pdfsets[iset], 
-                          '--makeResponse', '--semilep=1', '--pileup=ttbar', '--jerSys=0.1', '--use2Dcut',
-                          outname[isample]+name_pdfsets[iset]+name_pdfsysts[isyst]+"_2Dcut_nom_"+name_oddeven[ioe] ]
+                    s = [ 'python', 'runTruth.py', samples[isample], oddeven[ioe], pdfsysts[isyst], pdfsets[iset], 
+                          '--pileup=ttbar',
+                          outname[isample]+name_pdfsets[iset]+name_pdfsysts[isyst]+"_fullTruth"+name_oddeven[ioe] ]
                     if isample==0:
                         s.append('--mttGenMax=700.')
                     q.put(s)    
