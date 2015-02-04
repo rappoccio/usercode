90b3393bb7fc2b793ca75246f83da17fb4603c19
Louise Skinnari <louise.skinnari@cern.ch>
2015-02-04 14:40:57 +0100

    update nbr events for Q2 down

--- IHeartNY/test/makeHists.h
+++ IHeartNY/test/makeHists.h
@@ -290,7 +290,7 @@ SummedHist * getTTbarNonSemiLep( TString name, TString histname, bool doElectron
   double ttbar_nevents[nq2][nttbar] = {
     {21675970., 3082812., 1249111.},  // nominal
     {14983686., 2243672., 1241650.},  // q2 up
-    {1789004., 2170074., 1308090.}   // q2 down
+    {14545715., 2170074., 1308090.}   // q2 down
   };
   double ttbar_eff[nq2][nttbar] = {
     {1.0, 0.074, 0.015},  // nominal
