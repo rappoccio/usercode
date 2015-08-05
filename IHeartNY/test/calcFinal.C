f745d83b93e0badd481183bab819644b610e7c0e
Louise Skinnari <louise.skinnari@cern.ch>
2015-08-05 15:27:04 +0200

    fix MC cross sections

--- IHeartNY/test/calcFinal.C
+++ IHeartNY/test/calcFinal.C
@@ -1,31 +1,56 @@
 
 void calcFinal() {
 
-  float particle_nom[3] = {1.42312, 1.40652, 1.41482};
-  float parton_nom[3]   = {1.58565, 1.57645, 1.58105};
+  float particle_nom[3] = {1.46056, 1.44206, 0.};
+  float parton_nom[3]   = {1.62736, 1.61629, 0.};
+  particle_nom[2] = (particle_nom[0]+particle_nom[1])/2.;
+  parton_nom[2] = (parton_nom[0]+parton_nom[1])/2.;
 
+  float particle_pdfup[3] = {1.60231, 1.57759, 0.};
+  float parton_pdfup[3]   = {1.79039, 1.77282, 0.};
+  particle_pdfup[2] = (particle_pdfup[0]+particle_pdfup[1])/2.;
+  parton_pdfup[2] = (parton_pdfup[0]+parton_pdfup[1])/2.;
+  float particle_pdfdown[3] = {1.36372, 1.34908, 0.};
+  float parton_pdfdown[3]   = {1.50358, 1.49607, 0.};
+  particle_pdfdown[2] = (particle_pdfdown[0]+particle_pdfdown[1])/2.;
+  parton_pdfdown[2] = (parton_pdfdown[0]+parton_pdfdown[1])/2.;
+  float particle_scaleup[3] = {1.55751, 1.53262, 0.};
+  float parton_scaleup[3]   = {1.76479, 1.73796, 0.};
+  particle_scaleup[2] = (particle_scaleup[0]+particle_scaleup[1])/2.;
+  parton_scaleup[2] = (parton_scaleup[0]+parton_scaleup[1])/2.;
+  float particle_scaledown[3] = {1.3693, 1.31044, 0.};
+  float parton_scaledown[3]   = {1.4213, 1.40934, 0.};
+  particle_scaledown[2] = (particle_scaledown[0]+particle_scaledown[1])/2.;
+  parton_scaledown[2] = (parton_scaledown[0]+parton_scaledown[1])/2.;
+
+  float particle_MG[3] = {1.35881, 1.33966, 0.};
+  float parton_MG[3]   = {1.65839, 1.65651, 0.};
+  particle_MG[2] = (particle_MG[0]+particle_MG[1])/2.;
+  parton_MG[2] = (parton_MG[0]+parton_MG[1])/2.;
+
+  // beta_signal's from theta fit
   float data[3] = {0.65, 0.82, 0.88};
   float err_data[3] = {0.09, 0.09, 0.06};
-
-  float particle_pdfup[3] = {1.4808, 1.46017, 1.47049};
-  float parton_pdfup[3]   = {1.61083, 1.59747, 1.60415};
-  float particle_pdfdown[3] = {1.39423, 1.38017, 1.38720};
-  float parton_pdfdown[3]   = {1.56987, 1.56319, 1.56653};
-  float particle_scaleup[3] = {1.53471, 1.50954, 1.52212};
-  float parton_scaleup[3]   = {1.73895, 1.71178, 1.72537};
-  float particle_scaledown[3] = {1.35697, 1.29876, 1.32787};
-  float parton_scaledown[3]   = {1.40849, 1.39677, 1.40263};
-
-  float particle_MG[3] = {1.42186, 1.39482, 1.40834};
-  float parton_MG[3]   = {1.73534, 1.72472, 1.73003};
-
   float data_MG[3] = {0.64, 0.84, 0.87};
   float err_data_MG[3] = {0.09, 0.09, 0.06};
 
+  // additional luminosity uncertainty
   float lumi = 0.026;
+
+  std::cout.precision(4);
+
+  cout << endl << "*** MC cross sections***" << endl << endl;
+  cout << "CT10 nominal & " << parton_nom[2] << " & " << particle_nom[0] << " & " << particle_nom[1] << " & " << particle_nom[2] << " \\\\" << endl;
+  cout << "PDF up & " << parton_pdfup[2] << " & " << particle_pdfup[0] << " & " << particle_pdfup[1] << " & " << particle_pdfup[2] << " \\\\" << endl;
+  cout << "PDF down & " << parton_pdfdown[2] << " & " << particle_pdfdown[0] << " & " << particle_pdfdown[1] << " & " << particle_pdfdown[2] << " \\\\" << endl;
+  cout << "$Q^2$ up & " << parton_scaleup[2] << " & " << particle_scaleup[0] << " & " << particle_scaleup[1] << " & " << particle_scaleup[2] << " \\\\" << endl;
+  cout << "$Q^2$ down & " << parton_scaledown[2] << " & " << particle_scaledown[0] << " & " << particle_scaledown[1] << " & " << particle_scaledown[2] << " \\\\" << endl;
+  cout << "MadGraph & " << parton_MG[2] << " & " << particle_MG[0] << " & " << particle_MG[1] << " & " << particle_MG[2] << " \\\\" << endl;
+  cout << endl;
 
   std::cout.precision(3);
 
+  cout << endl << "*** POWHEG ***" << endl<< endl;
 
   for (int i=0; i<3; i++) {
 
@@ -39,11 +64,11 @@ void calcFinal() {
     float lum = particle_nom[i]*data[i]*lumi;
 
     cout << particle_nom[i]*data[i] << " +/- " << stat << " (stat) "
-	 << "+" << (particle_pdfup[i]-particle_nom[i])*data[i] << "" 
-	 << "/-" << (particle_nom[i]-particle_pdfdown[i])*data[i] << " (PDF) " 
+      // << "+" << (particle_pdfup[i]-particle_nom[i])*data[i] << "" 
+      // << "/-" << (particle_nom[i]-particle_pdfdown[i])*data[i] << " (PDF) " 
 	 << " +/- " << pdf << " (av. PDF) " 
-	 << "+" << (particle_scaleup[i]-particle_nom[i])*data[i] << "" 
-	 << "/-" << (particle_nom[i]-particle_scaledown[i])*data[i] << " (Q2) " 
+      // << "+" << (particle_scaleup[i]-particle_nom[i])*data[i] << "" 
+      // << "/-" << (particle_nom[i]-particle_scaledown[i])*data[i] << " (Q2) " 
 	 << " +/- " << q2 << " (av. Q2) " 
 	 << "+/- " << lum << " (lumi) "
 	 << " TOTAL uncertainty = " << sqrt(stat*stat + pdf*pdf + q2*q2 + lum*lum) 
@@ -59,11 +84,11 @@ void calcFinal() {
     lum = parton_nom[i]*data[i]*lumi;
 
     cout << parton_nom[i]*data[i] << " +/- " << stat << " (stat) "
-	 << "+" << (parton_pdfup[i]-parton_nom[i])*data[i] << "" 
-	 << "/-" << (parton_nom[i]-parton_pdfdown[i])*data[i] << " (PDF) " 
+      // << "+" << (parton_pdfup[i]-parton_nom[i])*data[i] << "" 
+      // << "/-" << (parton_nom[i]-parton_pdfdown[i])*data[i] << " (PDF) " 
 	 << " +/- " << pdf << " (av. PDF) " 
-	 << "+" << (parton_scaleup[i]-parton_nom[i])*data[i] << "" 
-	 << "/-" << (parton_nom[i]-parton_scaledown[i])*data[i] << " (Q2) " 
+      // << "+" << (parton_scaleup[i]-parton_nom[i])*data[i] << "" 
+      // << "/-" << (parton_nom[i]-parton_scaledown[i])*data[i] << " (Q2) " 
 	 << " +/- " << q2 << " (av. Q2) " 
 	 << "+/- " << lum << " (lumi) "
 	 << " TOTAL uncertainty = " << sqrt(stat*stat + pdf*pdf + q2*q2 + lum*lum) 
@@ -87,5 +112,6 @@ void calcFinal() {
 	 << "+/- " << parton_MG[i]*data_MG[i]*lumi << " (lumi) "
 	 << endl;
   }
+  cout << endl;
 
 }
--- IHeartNY/test/calcXS.C
+++ IHeartNY/test/calcXS.C
@@ -3,35 +3,42 @@ void calcOne(TString which, bool parton, bool doel);
 
 void calcXS() {
   
+  cout << endl;
   calcOne("CT10_nom",false,false);
   calcOne("CT10_nom",false,true);
   calcOne("CT10_nom",true,false);
   calcOne("CT10_nom",true,true);
 
+  cout << endl;
   calcOne("CT10_pdfup",false,false);
   calcOne("CT10_pdfup",false,true);
   calcOne("CT10_pdfup",true,false);
   calcOne("CT10_pdfup",true,true);
 
+  cout << endl;
   calcOne("CT10_pdfdown",false,false);
   calcOne("CT10_pdfdown",false,true);
   calcOne("CT10_pdfdown",true,false);
   calcOne("CT10_pdfdown",true,true);
 
+  cout << endl;
   calcOne("scaleup",false,false);
   calcOne("scaleup",false,true);
   calcOne("scaleup",true,false);
   calcOne("scaleup",true,true);
 
+  cout << endl;
   calcOne("scaledown",false,false);
   calcOne("scaledown",false,true);
   calcOne("scaledown",true,false);
   calcOne("scaledown",true,true);
 
+  cout << endl;
   calcOne("MG",false,false);
   calcOne("MG",false,true);
   calcOne("MG",true,false);
   calcOne("MG",true,true);
+  cout << endl;
 
 }
 
@@ -60,43 +67,31 @@ void calcOne(TString which, bool parton, bool doel) {
     TFile* fn3 = new TFile("histfiles_CT10_nom/2Dhists/TT_Mtt-1000toInf_CT10_TuneZ2star_8TeV-powheg-tauola_iheartNY_V1_mu_CT10_nom_2Dcut_nom.root");
   }
 
-  TString what = "ptGenTop_passParton";
-  if (!parton) what = "ptPartTop_passParticle";
+  TString whatDEN = "ptGenTop";
+  TString whatNUM = "ptGenTop_passParton";
+  if (!parton) whatNUM = "ptPartTop_passParticle";
 
   if (which=="MG") {
-    TH1F* h1 = (TH1F*) f1->Get(what);
+    TH1F* h1 = (TH1F*) f1->Get(whatDEN);
+    TH1F* h11 = (TH1F*) f1->Get(whatNUM);
   }
   else {
-    TH1F* h1 = (TH1F*) f1->Get(what);
-    TH1F* h2 = (TH1F*) f2->Get(what);
-    TH1F* h3 = (TH1F*) f3->Get(what);
+    TH1F* h1 = (TH1F*) f1->Get(whatDEN);
+    TH1F* h2 = (TH1F*) f2->Get(whatDEN);
+    TH1F* h3 = (TH1F*) f3->Get(whatDEN);
+    TH1F* h11 = (TH1F*) f1->Get(whatNUM);
+    TH1F* h22 = (TH1F*) f2->Get(whatNUM);
+    TH1F* h33 = (TH1F*) f3->Get(whatNUM);
   }
 
-  float norm1 = 1.0;
-  float norm2 = 1.0;
-  float norm3 = 1.0;
-
-  if (which=="CT10_pdfup" || which=="CT10_pdfdown") {
-    TH1F* h1_den = (TH1F*) f1->Get("mttbarGen0");
-    TH1F* h2_den = (TH1F*) f2->Get("mttbarGen0");
-    TH1F* h3_den = (TH1F*) f3->Get("mttbarGen0");
-    TH1F* h1_num = (TH1F*) fn1->Get("mttbarGen0");
-    TH1F* h2_num = (TH1F*) fn2->Get("mttbarGen0");
-    TH1F* h3_num = (TH1F*) fn3->Get("mttbarGen0");
-
-    norm1 = h1_num->GetSum()/h1_den->GetSum();
-    norm2 = h2_num->GetSum()/h2_den->GetSum();
-    norm3 = h3_num->GetSum()/h3_den->GetSum();
-
-    cout << "additional sample normalizations for " << which << " = " << norm1 << " (m<700), " << norm2 << " (700<m<1000), " << norm3 << " (m>1000), " << endl;
-  }
-
+  float normPDF = 1.0;
+  
   int iwhich = 0;
   if (which=="CT10_pdfup") iwhich = 1;
   else if (which=="CT10_pdfdown") iwhich = 2;
   else if (which=="scaleup") iwhich = 3;
   else if (which=="scaledown") iwhich = 4;
-
+  
   double ttbar_xs[5] = {
     245.8,  // nominal
     245.8,  // CT10 up
@@ -118,33 +113,75 @@ void calcOne(TString which, bool parton, bool doel) {
     {1.0, 0.074, 0.014},  // q2 up
     {1.0, 0.081, 0.016}   // q2 down
   };
-
+  
+  
   if (which=="MG") {
-    double ttbar_norm = 245.8/25424818.*4./9.;
+    double ttbar_norm = 245.8/25424818.*0.308;
     h1->Scale(ttbar_norm);
+    h11->Scale(ttbar_norm);
   }
   else {
+    
+    // correcting the inclusive cross section for PDF up/down is not really needed when taking the ratio in (pass pt>400)/(all)
+    if (which=="CT10_pdfup" || which=="CT10_pdfdown") {
+
+      TH1F* h1_den = (TH1F*) f1->Get("mttbarGen0");
+      TH1F* h2_den = (TH1F*) f2->Get("mttbarGen0");
+      TH1F* h3_den = (TH1F*) f3->Get("mttbarGen0");
+      TH1F* h1_num = (TH1F*) fn1->Get("mttbarGen0");
+      TH1F* h2_num = (TH1F*) fn2->Get("mttbarGen0");
+      TH1F* h3_num = (TH1F*) fn3->Get("mttbarGen0");
+
+      h1_num->Sumw2();
+      h2_num->Sumw2();
+      h3_num->Sumw2();
+      h1_num->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][0] / ttbar_nevents[iwhich][0]); 
+      h2_num->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][1] / ttbar_nevents[iwhich][1]); 
+      h3_num->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][2] / ttbar_nevents[iwhich][2]); 
+      h1_num->Add(h2_num);
+      h1_num->Add(h3_num);
+      h1_den->Sumw2();
+      h2_den->Sumw2();
+      h3_den->Sumw2();
+      h1_den->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][0] / ttbar_nevents[iwhich][0]); 
+      h2_den->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][1] / ttbar_nevents[iwhich][1]); 
+      h3_den->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][2] / ttbar_nevents[iwhich][2]); 
+      h1_den->Add(h2_den);
+      h1_den->Add(h3_den);
+      
+      normPDF = h1_num->GetSum()/h1_den->GetSum();
+      //cout << "additional sample normalization for " << which << " = " << normPDF << endl;
+    }
+
     h1->Sumw2();
     h2->Sumw2();
     h3->Sumw2();
-    
-    h1->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][0] * norm1 / ttbar_nevents[iwhich][0]); 
-    h2->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][1] * norm2 / ttbar_nevents[iwhich][1]); 
-    h3->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][2] * norm3 / ttbar_nevents[iwhich][2]); 
-    
+    h1->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][0] * normPDF / ttbar_nevents[iwhich][0]); 
+    h2->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][1] * normPDF / ttbar_nevents[iwhich][1]); 
+    h3->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][2] * normPDF / ttbar_nevents[iwhich][2]); 
     h1->Add(h2);
     h1->Add(h3);
+    
+    h11->Sumw2();
+    h22->Sumw2();
+    h33->Sumw2();
+    h11->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][0] * normPDF / ttbar_nevents[iwhich][0]); 
+    h22->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][1] * normPDF / ttbar_nevents[iwhich][1]); 
+    h33->Scale(ttbar_xs[iwhich] * ttbar_eff[iwhich][2] * normPDF / ttbar_nevents[iwhich][2]); 
+    h11->Add(h22);
+    h11->Add(h33);
   }
-
+  
   TString level = "particle";
   if (parton) level = "parton";
   TString lepton = "muon";
   if (doel) lepton = "electron";
-
-  cout << endl;
-  cout << "Inclusive cross section at " << level << " level for " << which << " = " << h1->GetSum()*27/4 << " pb (" << lepton << ")" << endl;
-  cout << endl;
   
+  
+  float total_xsec = ttbar_xs[iwhich];
+  if (which=="MG") total_xsec = total_xsec*0.308/(1./3.);
+
+  cout << "Inclusive cross section at " << level << " level for " << which << " = " << total_xsec*h11->GetSum()/h1->GetSum() << " pb (" << lepton << ")" << endl;  
 
 }
 
